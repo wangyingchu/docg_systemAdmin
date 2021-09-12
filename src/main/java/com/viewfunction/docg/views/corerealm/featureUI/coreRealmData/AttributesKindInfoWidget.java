9fe7b152a148f7ff271361d4cf1460d81b6e5bbf
wangyingchu <yingchuwang@gmail.com>
2021-09-12 22:17:18 +0800

    update for CoreRealmDataUI feature

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/AttributesKindInfoWidget.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.coreRealmData;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class AttributesKindInfoWidget extends VerticalLayout {
+}
---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/ClassificationInfoWidget.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.coreRealmData;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class ClassificationInfoWidget extends VerticalLayout {
+}
