e6b350e045e8dd07ea766fde15b216a982fa17fe
wangyingchu <yingchuwang@gmail.com>
2021-09-21 22:03:23 +0800

    update for CoreRealmDataUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/ClassificationInfoWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/ClassificationInfoWidget.java
@@ -1,12 +1,18 @@
 package com.viewfunction.docg.views.corerealm.featureUI.coreRealmData;
 
 import com.github.appreciated.apexcharts.ApexCharts;
+import com.storedobject.chart.SOChart;
+import com.storedobject.chart.Title;
+import com.storedobject.chart.TreeChart;
+import com.storedobject.chart.TreeData;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.viewfunction.docg.element.commonComponent.PrimaryKeyValueDisplayItem;
 import com.viewfunction.docg.element.commonComponent.SecondaryKeyValueDisplayItem;
+
+import java.util.Random;
 
 public class ClassificationInfoWidget extends HorizontalLayout {
 
@@ -67,13 +73,64 @@ public class ClassificationInfoWidget extends HorizontalLayout {
         add(rightComponentContainer);
         this.setFlexGrow(1,rightComponentContainer);
 
+
+
+
+
+
+
+
+
+
+
+
+        // Creating a chart display area
+        SOChart soChart = new SOChart();
+        soChart.setSize("800px", "500px");
+
+// Tree chart
+// (By default it assumes circular shape. Otherwise, we can set orientation)
+// All values are randomly generated
+        TreeChart tc = new TreeChart();
+        TreeData td = new TreeData("Root", 1000);
+        tc.setTreeData(td);
+        Random r = new Random();
+        for(int i = 1; i < 21; i++) {
+            td.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+        TreeData td1 = td.get(13);
+        td = td.get(9);
+        for(int i = 50; i < 56; i++) {
+            td.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+        for(int i = 30; i < 34; i++) {
+            td1.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+
+// Add to the chart display area with a simple title
+        soChart.add(tc, new Title("A Circular Tree Chart"));
+
+// Finally, add it to my layout
+
+
+
+
+
+
+
+
+
+
+
+
+
         ApexCharts apexCharts = new ConceptionEntityCountChart()
                 .withColors("#168eea", "#ee4f4f", "#03a9f4", "#76b852", "#323b43", "#59626a", "#0288d1", "#ffc107", "#d32f2f", "#00d1b2","#ced7df").build();
         //https://www.materialpalette.com/
         //https://materialui.co/colors/
         //http://brandcolors.net/
         apexCharts.setWidth("330");
-        rightComponentContainer.add(apexCharts);
+        rightComponentContainer.add(soChart);
         rightComponentContainer.setHorizontalComponentAlignment(Alignment.START,apexCharts);
     }
 }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/ClassificationsTreeChart.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/ClassificationsTreeChart.java
@@ -1,4 +1,48 @@
 package com.viewfunction.docg.views.corerealm.featureUI.coreRealmData;
 
+import com.storedobject.chart.SOChart;
+import com.storedobject.chart.Title;
+import com.storedobject.chart.TreeChart;
+import com.storedobject.chart.TreeData;
+
+import java.util.Random;
+
 public class ClassificationsTreeChart {
+
+    public ClassificationsTreeChart(){
+
+
+
+        // Creating a chart display area
+        SOChart soChart = new SOChart();
+        soChart.setSize("800px", "500px");
+
+// Tree chart
+// (By default it assumes circular shape. Otherwise, we can set orientation)
+// All values are randomly generated
+        TreeChart tc = new TreeChart();
+        TreeData td = new TreeData("Root", 1000);
+        tc.setTreeData(td);
+        Random r = new Random();
+        for(int i = 1; i < 21; i++) {
+            td.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+        TreeData td1 = td.get(13);
+        td = td.get(9);
+        for(int i = 50; i < 56; i++) {
+            td.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+        for(int i = 30; i < 34; i++) {
+            td1.add(new TreeData("Node " + i, r.nextInt(500)));
+        }
+
+// Add to the chart display area with a simple title
+        soChart.add(tc, new Title("A Circular Tree Chart"));
+
+// Finally, add it to my layout
+
+
+
+
+    }
 }
