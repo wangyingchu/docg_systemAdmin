d02f086adc44c7e6eb615fd2b3054871ff5dff02
wangyingchu <yingchuwang@gmail.com>
2022-05-03 20:31:27 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/element/commonComponent/SectionActionBar.java
+++ src/main/java/com/viewfunction/docg/element/commonComponent/SectionActionBar.java
@@ -3,7 +3,6 @@ package com.viewfunction.docg.element.commonComponent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -24,21 +23,8 @@ public class SectionActionBar extends HorizontalLayout {
             this.setVerticalComponentAlignment(Alignment.CENTER,sectionIcon);
         }
 
-
-
-
-
-        //add(sectionTitleLabel);
-
-        //this.setFlexGrow(1,sectionTitleLabel);
-
-
-
-
         HorizontalLayout titleElementsContainer = new HorizontalLayout();
-
         Label sectionTitleLabel = new Label(sectionTitle);
-        //sectionTitleLabel.getStyle().set("font-size","var(--lumo-font-size-s)");
         sectionTitleLabel.addClassNames("text-s","font-semibold");
 
         titleElementsContainer.add(sectionTitleLabel);
@@ -48,25 +34,13 @@ public class SectionActionBar extends HorizontalLayout {
         this.setVerticalComponentAlignment(Alignment.CENTER,titleElementsContainer);
         titleElementsContainer.setDefaultVerticalComponentAlignment(Alignment.CENTER);
 
-
-
-
-
-
-
-
         if(actionComponentsList != null){
             for(Component currentComponent:actionComponentsList){
                 this.add(currentComponent);
                 this.setVerticalComponentAlignment(Alignment.START,currentComponent);
-                //Span spacingSpan = new Span(" | ");
-               // spacingSpan.setWidth(10,Unit.PIXELS);
-                //this.add(spacingSpan);
-
-                Icon divIcon = new Icon(VaadinIcon.ALIGN_CENTER);
+                Icon divIcon = new Icon(VaadinIcon.LINE_V);
                 divIcon.setSize("8px");
                 this.add(divIcon);
-
                 this.setVerticalComponentAlignment(Alignment.CENTER,divIcon);
             }
         }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/ConceptionKindManagementUI.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/ConceptionKindManagementUI.java
@@ -1,6 +1,8 @@
 package com.viewfunction.docg.views.corerealm.featureUI;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -28,50 +30,6 @@ import java.util.List;
 public class ConceptionKindManagementUI extends VerticalLayout {
 
     public ConceptionKindManagementUI(){
-        /*
-        add(new Label("ConceptionKindManagementUI"));
-        H3 personalTitle = new H3("Personal information");
-        personalTitle.setId("PERSONAL_TITLE_ID");
-
-        TextField firstName = new TextField("First name");
-        firstName.setWidthFull();
-
-        TextField lastName = new TextField("Last name");
-        lastName.setWidthFull();
-
-        DatePicker birthDate = new DatePicker("Birthdate");
-        birthDate.setInitialPosition(LocalDate.of(1990, 1, 1));
-        birthDate.setWidthFull();
-
-        Section personalInformation = new Section(personalTitle, firstName, lastName, birthDate);
-        personalInformation.getElement().setAttribute("aria-labelledby", "PERSONAL_TITLE_ID");
-
-        // Employment information
-        H3 employementTitle = new H3("Employment information");
-        employementTitle.setId("EMPLOYMENT_TITLE_ID");
-
-        TextField position = new TextField("Position");
-        position.setWidthFull();
-
-        TextArea additionalInformation = new TextArea("Additional Information");
-        additionalInformation.setWidthFull();
-
-        Section employmentInformation = new Section(employementTitle, position, additionalInformation);
-        employmentInformation.getElement().setAttribute("aria-labelledby", "EMPLOYMENT_TITLE_ID");
-
-        // NOTE
-        // We are using inline styles here to keep the example simple.
-        // We recommend placing CSS in a separate style sheet and to
-        // encapsulating the styling in a new component.
-        Scroller scroller = new Scroller(new Div(personalInformation, employmentInformation));
-        scroller.setHeight(150, Unit.PIXELS);
-        scroller.setScrollDirection(Scroller.ScrollDirection.VERTICAL);
-        scroller.getStyle()
-                .set("border-bottom", "1px solid var(--lumo-contrast-20pct)")
-                .set("padding", "var(--lumo-space-m)");
-        add(scroller);
-        */
-
 
         Button refreshDataButton = new Button("刷新概念类型数据统计信息",new Icon(VaadinIcon.REFRESH));
         refreshDataButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
@@ -109,14 +67,20 @@ public class ConceptionKindManagementUI extends VerticalLayout {
         createConceptionKindButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
         conceptionKindManagementOperationButtonList.add(createConceptionKindButton);
 
-
-
         Icon icon = new Icon(VaadinIcon.LIST);
         SectionActionBar sectionActionBar = new SectionActionBar(icon,"概念类型定义:",conceptionKindManagementOperationButtonList);
         add(sectionActionBar);
+    }
 
-
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
         loadConceptionKindsInfo();
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        super.onDetach(detachEvent);
     }
 
     private void loadConceptionKindsInfo(){
