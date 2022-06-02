ba55d7ca6026127a8cd8796e1823e066f3a647f4
wangyingchu <yingchuwang@gmail.com>
2022-06-03 00:11:33 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/ConceptionKindsCorrelationInfoSummaryChart.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/ConceptionKindsCorrelationInfoSummaryChart.java
@@ -1,9 +1,12 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement;
 
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import com.vaadin.flow.component.page.Page;
+import com.vaadin.flow.dom.Element;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionKindCorrelationInfo;
 import com.vaadin.flow.component.html.IFrame;
 
@@ -22,7 +25,20 @@ public class ConceptionKindsCorrelationInfoSummaryChart extends VerticalLayout {
     }
 
     public void loadConceptionKindCorrelationInfo(){
-        //UI.getCurrent().getPage();
+        UI.getCurrent().getPage().fetchCurrentURL(currentUrl -> {
+            // This is your own method that you may do something with the url.
+            // Please note that this method runs asynchronously
+            //storeCurrentURL(currentUrl);
+        });
+
+        UI.getCurrent().getPage().retrieveExtendedClientDetails(details -> {
+            // This is your own method that you may do something with the screen width.
+            // Please note that this method runs asynchronously
+            //handleScreenWidth(details.getScreenWidth());
+        });
+
+
+
         IFrame _IFrame = new IFrame();
         _IFrame.getStyle().set("border","0");
         _IFrame.setSrc("http://192.168.3.7:7141/instanceRelationsExplore/U3BhY2VOYW1lW0NJTURhdGFEaXNjb3ZlckVuZ2luZVVUXU9iamVjdFR5cGVbQ2ltT2JqZWN0VHlwZVNUQVRfMDAxXUluc3RhbmNlUklEWyMxNTc6N10=");
@@ -30,6 +46,17 @@ public class ConceptionKindsCorrelationInfoSummaryChart extends VerticalLayout {
         _IFrame.setWidth(950,Unit.PIXELS);
         add(_IFrame);
     }
+
+    public static void logElementSize(String name,
+                                      Element element) {
+        Page page = UI.getCurrent().getPage();
+
+        page.executeJs(
+                "console.log($0 + ' size:', "
+                        + "$1.offsetWidth, $1.offsetHeight)",
+                name, element);
+    }
+
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
