842f9d3b83f176eaefbc6d552e40e324dce28722
wangyingchu <yingchuwang@gmail.com>
2022-06-02 23:28:58 +0800

    update application PWA name

--- src/main/java/com/viewfunction/docg/ApplicationLaunchpad.java
+++ src/main/java/com/viewfunction/docg/ApplicationLaunchpad.java
@@ -16,7 +16,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 @NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @Theme("docg-pac")
-@PWA(name = "My Todo", shortName = "My Todo", offlineResources = {})
+@PWA(name = "DOCG Platform System Administrator Console", shortName = "DOCG-PAC", offlineResources = {})
 public class ApplicationLaunchpad extends SpringBootServletInitializer implements AppShellConfigurator{
 
     public static void main(String[] args) {
