d824390b6bba49bb1f2467a4efe8ee3284e302df
wangyingchu <yingchuwang@gmail.com>
2022-06-29 00:03:04 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionKindQueryResultsView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionKindQueryResultsView.java
@@ -1,24 +1,48 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.queryConceptionKind;
 
+import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.shared.Registration;
 import com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query.QueryParameters;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntitiesRetrieveResult;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
+import com.viewfunction.docg.element.commonComponent.SecondaryIconTitle;
+import com.viewfunction.docg.element.commonComponent.SecondaryKeyValueDisplayItem;
 import com.viewfunction.docg.element.eventHandling.ConceptionKindQueriedEvent;
 import com.viewfunction.docg.util.ResourceHolder;
 
 public class ConceptionKindQueryResultsView extends VerticalLayout implements
-        ConceptionKindQueriedEvent.ConceptionKindQueriedListener{
-
+        ConceptionKindQueriedEvent.ConceptionKindQueriedListener {
     private String conceptionKindName;
-
+    private Registration listener;
+    private Grid<ConceptionEntity> queryResultGrid;
     public ConceptionKindQueryResultsView(String conceptionKindName){
         this.conceptionKindName = conceptionKindName;
+        this.setPadding(true);
+        this.setSpacing(true);
+
+        HorizontalLayout titleLayout = new HorizontalLayout();
+        add(titleLayout);
+        SecondaryIconTitle filterTitle2 = new SecondaryIconTitle(new Icon(VaadinIcon.HARDDRIVE_O),"查询结果");
+        titleLayout.add(filterTitle2);
+        new SecondaryKeyValueDisplayItem(titleLayout, FontAwesome.Regular.CLOCK.create(),"查询开始时间","1,000,000,000");
+        new SecondaryKeyValueDisplayItem(titleLayout, FontAwesome.Regular.CLOCK.create(),"查询结束时间","1,000,000,000");
+        new SecondaryKeyValueDisplayItem(titleLayout, VaadinIcon.LIST_OL.create(),"结果集数据量","1,000,000,000");
+
+        queryResultGrid = new Grid<>();
+        queryResultGrid.setWidth(100,Unit.PERCENTAGE);
+        add(queryResultGrid);
     }
 
     @Override
@@ -41,11 +65,21 @@ public class ConceptionKindQueryResultsView extends VerticalLayout implements
     protected void onAttach(AttachEvent attachEvent) {
         super.onAttach(attachEvent);
         ResourceHolder.getApplicationBlackboard().addListener(this);
+        // Add browser window listener to observe size change
+        getUI().ifPresent(ui -> listener = ui.getPage().addBrowserWindowResizeListener(event -> {
+            queryResultGrid.setHeight(event.getHeight()-140,Unit.PIXELS);
+        }));
+        // Adjust size according to initial width of the screen
+        getUI().ifPresent(ui -> ui.getPage().retrieveExtendedClientDetails(receiver -> {
+            int browserHeight = receiver.getBodyClientHeight();
+            queryResultGrid.setHeight(browserHeight-140,Unit.PIXELS);
+        }));
     }
 
     @Override
     protected void onDetach(DetachEvent detachEvent) {
         // Listener needs to be eventually removed in order to avoid resource leak
+        listener.remove();
         super.onDetach(detachEvent);
         ResourceHolder.getApplicationBlackboard().removeListener(this);
     }
