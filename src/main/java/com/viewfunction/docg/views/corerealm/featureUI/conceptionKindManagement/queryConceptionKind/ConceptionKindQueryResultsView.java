2d935892c7c52a5c442bd8518128acd9d21a1f4f
wangyingchu <yingchuwang@gmail.com>
2022-06-30 12:55:22 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionKindQueryResultsView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionKindQueryResultsView.java
@@ -4,18 +4,19 @@ import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.function.ValueProvider;
 import com.vaadin.flow.shared.Registration;
 import com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query.QueryParameters;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntitiesAttributesRetrieveResult;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntityValue;
-import com.viewfunction.docg.coreRealm.realmServiceCore.payload.EntityStatisticsInfo;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
@@ -35,7 +36,7 @@ public class ConceptionKindQueryResultsView extends VerticalLayout implements
         ConceptionKindQueriedEvent.ConceptionKindQueriedListener {
     private String conceptionKindName;
     private Registration listener;
-    private Grid<Map<String, Object>> queryResultGrid;
+    private Grid<ConceptionEntityValue> queryResultGrid;
     private SecondaryKeyValueDisplayItem startTimeDisplayItem;
     private SecondaryKeyValueDisplayItem finishTimeDisplayItem;
     private SecondaryKeyValueDisplayItem dataCountDisplayItem;
@@ -56,7 +57,17 @@ public class ConceptionKindQueryResultsView extends VerticalLayout implements
         queryResultGrid = new Grid<>();
         queryResultGrid.setWidth(100,Unit.PERCENTAGE);
 
-
+        queryResultGrid.addColumn(ConceptionEntityValue::getConceptionEntityUID).setHeader(" EntityUID").setId("IDX");
+                //.setWidth(90).setResizable(true);
+        queryResultGrid.addComponentColumn(new ConceptionEntityActionButtonsValueProvider()).setHeader("操作").setId("ACTIONS");
+            //.setWidth(150).setResizable(false);
+           // queryResultGrid.addColumn(InfoObjectValueVO::getObjectInstanceRID).setCaption(" ObjectInstanceRID").setId("0").setWidth(140).setResizable(false);
+        queryResultGrid.addColumn(new ValueProvider<ConceptionEntityValue, Object>() {
+            @Override
+            public Object apply(ConceptionEntityValue conceptionEntityValue) {
+                return null;
+            }
+        }).setHeader(" ID").setId("1");
         //queryResultGrid.addColumn("EntityStatisticsInfo::getEntityKindName").setHeader("概念类型名称").setKey("idx_0");
         add(queryResultGrid);
     }
@@ -86,16 +97,34 @@ public class ConceptionKindQueryResultsView extends VerticalLayout implements
                     String finishTimeStr = finishZonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime((FormatStyle.MEDIUM)));
                     finishTimeDisplayItem.updateDisplayValue(finishTimeStr);
                     dataCountDisplayItem.updateDisplayValue(""+conceptionEntitiesAttributesRetrieveResult.getOperationStatistics().getResultEntitiesCount());
+
+                    List<ConceptionEntityValue> conceptionEntityValueList = conceptionEntitiesAttributesRetrieveResult.getConceptionEntityValues();
+                    List<String> entityAttributeNamesList = new ArrayList<>();
+                    entityAttributeNamesList.add("Prop001");
+                    entityAttributeNamesList.add("Prop002");
+                    entityAttributeNamesList.add("Prop003");
+
+                    for(ConceptionEntityValue currentConceptionEntityValue:conceptionEntityValueList){
+                        currentConceptionEntityValue.getEntityAttributesValue().put("Prop001",1);
+                        currentConceptionEntityValue.getEntityAttributesValue().put("Prop002","sssssssss");
+                                currentConceptionEntityValue.getEntityAttributesValue().put("Prop003",new Date());
+                    }
+
+                    if (entityAttributeNamesList != null && entityAttributeNamesList.size() > 0) {
+                        for (String currentProperty : entityAttributeNamesList) {
+                            //if (!currentProperty.equals(BusinessLogicConstant.ID_PROPERTY_TYPE_NAME)) {
+                            queryResultGrid.addColumn(new ValueProvider<ConceptionEntityValue, Object>() {
+                                    @Override
+                                    public Object apply(ConceptionEntityValue conceptionEntityValue) {
+                                        return conceptionEntityValue.getEntityAttributesValue().get(currentProperty);
+                                    }
+                                }).setHeader(" " + currentProperty).setId(currentProperty + "_ID");
+                               // this.currentDisplayPropertiesList.add(currentProperty + "_ID");
+                            //}
+                        }
+                    }
+                    queryResultGrid.setItems(conceptionEntityValueList);
                 }
-
-
-
-
-
-
-
-
-
             } catch (CoreRealmServiceEntityExploreException e) {
                 throw new RuntimeException(e);
             }
@@ -125,198 +154,20 @@ public class ConceptionKindQueryResultsView extends VerticalLayout implements
         ResourceHolder.getApplicationBlackboard().removeListener(this);
     }
 
-
-
-
-
-    public class MyDataProvider<T> extends ListDataProvider<T> {
-
-        public MyDataProvider(Collection<T> items) {
-            super(items);
-        }
+    private class ConceptionEntityActionButtonsValueProvider implements ValueProvider<ConceptionEntityValue,HorizontalLayout>{
 
         @Override
-        public String getId(T item) {
-            Objects.requireNonNull(item,
-                    "Cannot provide an id for a null item.");
-            if (item instanceof Map<?, ?>) {
-                if (((Map<String, ?>) item).get("id") != null)
-                    return ((Map<String, ?>) item).get("id").toString();
-                else
-                    return item.toString();
-            } else {
-                return item.toString();
-            }
+        public HorizontalLayout apply(ConceptionEntityValue o) {
+            HorizontalLayout actionButtonContainerLayout = new HorizontalLayout();
+            actionButtonContainerLayout.setMargin(false);
+            actionButtonContainerLayout.setSpacing(false);
+            Button linkButton = new Button();
+            linkButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+            linkButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+            linkButton.setIcon(VaadinIcon.EYE.create());
+            //linkButton.setDescription("显示对象实例数据详情");
+            actionButtonContainerLayout.add(linkButton);
+            return actionButtonContainerLayout;
         }
-
-    }
-
-
-
-
-
-
-
-
-}
-/*   https://cookbook.vaadin.com/grid-with-map
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Random;
-
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.IntegerField;
-import com.vaadin.flow.component.textfield.TextFieldVariant;
-import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.provider.ListDataProvider;
-import com.vaadin.flow.router.Route;
-
-@Route("grid-with-map")
-@CssImport(value = "./recipe/mapingridandbinder/mapingridandbinder.css", themeFor = "vaadin-grid")
-public class MapInGridAndBinder extends VerticalLayout {
-
-    public MapInGridAndBinder() {
-        setHeight("500px");
-        IntegerField field = new IntegerField("Columns ");
-        field.setMin(5);
-        field.setMax(25);
-        field.setHasControls(true);
-        VerticalLayout container = new VerticalLayout();
-        container.setSizeFull();
-        add(field, container);
-
-        field.addValueChangeListener(event -> {
-            Integer value = event.getValue();
-            container.removeAll();
-            Grid<Map<String, Integer>> grid = createGrid(value);
-            grid.setSizeFull();
-            container.add(grid);
-        });
-
-        field.setValue(10);
-    }
-
-    private Grid<Map<String, Integer>> createGrid(int columns) {
-        // Instantiate Grid with Map type
-        Grid<Map<String, Integer>> grid = new Grid<>();
-
-        List<Map<String, Integer>> items = createData(columns);
-        MyDataProvider<Map<String, Integer>> dataProvider = new MyDataProvider<>(
-                items);
-        grid.setItems(dataProvider);
-
-        // Instantiate Binder with Map type
-        Binder<Map<String, Integer>> binder = new Binder<>();
-
-        grid.addColumn(map -> map.get("id")).setHeader("Id");
-
-        // Generate requested amount of columns and setup Binder for edit fields
-        for (int i = 0; i < columns; i++) {
-            final int index = i;
-            IntegerField integerField = new IntegerField();
-            binder.forField(integerField).bind(map -> map.get("col" + index),
-                    (map, value) -> map.put("col" + index, value));
-            grid.addColumn(map -> map.get("col" + index))
-                    .setEditorComponent(integerField).setHeader("Col " + index);
-            // Use small variant for the field in order to edit row look nicer
-            integerField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
-            integerField.addKeyDownListener(Key.ENTER, event -> {
-                grid.getEditor().closeEditor();
-            });
-            integerField.addKeyDownListener(Key.ESCAPE, event -> {
-                grid.getEditor().closeEditor();
-            });
-        }
-        grid.getEditor().setBinder(binder);
-
-        // Add column for the sum
-        grid.addColumn(map -> map.get("sum")).setHeader("Sum")
-                .setClassNameGenerator(item -> "sum");
-
-        // Tip: Lets use item click listener as toggle for editing
-        grid.addItemClickListener(event -> {
-            Map<String, Integer> item = event.getItem();
-            // If editor is open already, lets close it
-            if (grid.getEditor().isOpen()
-                    && grid.getEditor().getItem().equals(item)) {
-                grid.getEditor().closeEditor();
-            } else {
-                grid.getEditor().editItem(item);
-                // Tip: Find the field from the colum and focus it for better UX
-                IntegerField field = (IntegerField) event.getColumn()
-                        .getEditorComponent();
-                field.focus();
-            }
-        });
-
-        // Update sum when editor is closed
-        grid.getEditor().addCloseListener(event -> {
-            Map<String, Integer> item = event.getItem();
-            int sum = 0;
-            for (int i = 0; i < columns; i++) {
-                sum += item.get("col" + i);
-            }
-            item.put("sum", sum);
-            // Item refrsh is needed in order to new sum be visible
-            grid.getDataProvider().refreshItem(item);
-        });
-
-        // Tip: Grid uses these values for padding, lets set them smaller value
-        // so that editor will look nicer.
-        grid.getStyle().set("--lumo-space-xs", "1px");
-        grid.getStyle().set("--lumo-space-m", "1px");
-        return grid;
-    }
-
-    // Generate some mock data
-    private List<Map<String, Integer>> createData(int columns) {
-        Random random = new Random();
-        List<Map<String, Integer>> items = new ArrayList<>();
-        for (int j = 0; j < 1000; j++) {
-            final Map<String, Integer> values = new HashMap<>();
-            values.put("id", j);
-            int sum = 0;
-            for (int i = 0; i < columns; i++) {
-                int number = random.nextInt(10000);
-                values.put("col" + i, number);
-                sum = sum + number;
-            }
-            values.put("sum", sum);
-            items.add(values);
-        }
-        return items;
-    }
-
-    // HashMap identity does not play nice with Grid by default, thus
-    // we use option to override getId method of DataProvider to return value
-    // of "id" key as identity.
-    public class MyDataProvider<T> extends ListDataProvider<T> {
-
-        public MyDataProvider(Collection<T> items) {
-            super(items);
-        }
-
-        @Override
-        public String getId(T item) {
-            Objects.requireNonNull(item,
-                    "Cannot provide an id for a null item.");
-            if (item instanceof Map<?, ?>) {
-                if (((Map<String, ?>) item).get("id") != null)
-                    return ((Map<String, ?>) item).get("id").toString();
-                else
-                    return item.toString();
-            } else {
-                return item.toString();
-            }
-        }
-
     }
 }
-* */
