a6c21a88b7a9dcf5769f8ca183c8e4c179f2dd0d
wangyingchu <yingchuwang@gmail.com>
2022-07-29 09:42:22 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/ConceptionEntityMetaInfoView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/ConceptionEntityMetaInfoView.java
@@ -1,20 +1,38 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.viewfunction.docg.coreRealm.realmServiceCore.feature.MetaAttributeFeatureSupportable;
+import com.viewfunction.docg.coreRealm.realmServiceCore.feature.spi.neo4j.featureImpl.Neo4JAttributesMeasurableImpl;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.neo4j.termImpl.Neo4JConceptionEntityImpl;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import com.viewfunction.docg.element.commonComponent.FootprintMessageBar;
+import com.viewfunction.docg.element.userInterfaceUtil.CommonUIOperationUtil;
 
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ConceptionEntityMetaInfoView  extends VerticalLayout {
     private String conceptionKind;
     private String conceptionEntityUID;
-
+    private DateTimePicker createDate;
+    private DateTimePicker lastUpdateDate;
+    private TextField dataSource;
+    private TextField creator;
     public ConceptionEntityMetaInfoView(String conceptionKind,String conceptionEntityUID){
         this.setMargin(false);
         this.setSpacing(false);
@@ -32,10 +50,14 @@ public class ConceptionEntityMetaInfoView  extends VerticalLayout {
         FootprintMessageBar entityInfoFootprintMessageBar = new FootprintMessageBar(footprintMessageVOList);
         add(entityInfoFootprintMessageBar);
 
-        DateTimePicker createDate = new DateTimePicker("创建时间");
-        DateTimePicker lastUpdateDate = new DateTimePicker("最后更新时间");
-        TextField dataSource = new TextField("数据原始来源");
-        TextField creator = new TextField("创建人ID");
+        createDate = new DateTimePicker("创建时间");
+        createDate.setReadOnly(true);
+        lastUpdateDate = new DateTimePicker("最后更新时间");
+        lastUpdateDate.setReadOnly(true);
+        dataSource = new TextField("数据原始来源");
+        dataSource.setReadOnly(true);
+        creator = new TextField("创建人ID");
+        creator.setReadOnly(true);
 
         FormLayout formLayout = new FormLayout();
         formLayout.add(createDate, lastUpdateDate,dataSource,creator);
@@ -46,5 +68,42 @@ public class ConceptionEntityMetaInfoView  extends VerticalLayout {
                 new FormLayout.ResponsiveStep("500px", 2)
         );
         add(formLayout);
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+        coreRealm.openGlobalSession();
+        ConceptionKind targetConceptionKind = coreRealm.getConceptionKind(this.conceptionKind);
+        if(targetConceptionKind != null){
+            ConceptionEntity targetEntity = targetConceptionKind.getEntityByUID(this.conceptionEntityUID);
+            if(targetEntity != null){
+                ZoneId zoneId = ZoneId.systemDefault();
+                if(targetEntity.hasAttribute(RealmConstant._createDateProperty)){
+                    Date createdDateValue = (Date)targetEntity.getAttribute(RealmConstant._createDateProperty).getAttributeValue();
+                    Instant instant = createdDateValue.toInstant();
+                    createDate.setValue(instant.atZone(zoneId).toLocalDateTime());
+                }
+                if(targetEntity.hasAttribute(RealmConstant._lastModifyDateProperty)){
+                    Date lastModifyDateValue = (Date)targetEntity.getAttribute(RealmConstant._lastModifyDateProperty).getAttributeValue();
+                    Instant instant = lastModifyDateValue.toInstant();
+                    lastUpdateDate.setValue(instant.atZone(zoneId).toLocalDateTime());
+                }
+                if(targetEntity.hasAttribute(RealmConstant._creatorIdProperty)){
+                    String creatorIdValue = targetEntity.getAttribute(RealmConstant._creatorIdProperty).getAttributeValue().toString();
+                    creator.setValue(creatorIdValue);
+                }
+                if(targetEntity.hasAttribute(RealmConstant._dataOriginProperty)){
+                    String dataOriginValue = targetEntity.getAttribute(RealmConstant._dataOriginProperty).getAttributeValue().toString();
+                    dataSource.setValue(dataOriginValue);
+                }
+            }else{
+                CommonUIOperationUtil.showPopupNotification("概念类型 "+conceptionKind+" 中不存在 UID 为"+conceptionEntityUID+" 的概念实体", NotificationVariant.LUMO_ERROR);
+            }
+        }else{
+            CommonUIOperationUtil.showPopupNotification("概念类型 "+conceptionKind+" 不存在", NotificationVariant.LUMO_ERROR);
+        }
+        coreRealm.closeGlobalSession();
     }
 }
