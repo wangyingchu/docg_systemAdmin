47af4ce75789ef848ed75f5ebf12a9e1b0435b44
wangyingchu <yingchuwang@gmail.com>
2022-07-30 23:10:05 +0800

    update for ConceptionKind Management features

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/ConceptionEntityRelationInfoView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/ConceptionEntityRelationInfoView.java
@@ -1,12 +1,54 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import com.viewfunction.docg.element.commonComponent.SecondaryTitleActionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConceptionEntityRelationInfoView extends VerticalLayout {
     private String conceptionKind;
     private String conceptionEntityUID;
     public ConceptionEntityRelationInfoView(String conceptionKind,String conceptionEntityUID){
+        this.setPadding(false);
         this.conceptionKind = conceptionKind;
         this.conceptionEntityUID = conceptionEntityUID;
+        List<Component> secondaryTitleComponentsList = new ArrayList<>();
+        List<Component> actionComponentsList = new ArrayList<>();
+
+        Button createRelationButton= new Button("新建数据关联");
+        createRelationButton.setIcon(VaadinIcon.LINK.create());
+        createRelationButton.addThemeVariants(ButtonVariant.LUMO_ICON,ButtonVariant.LUMO_SMALL,ButtonVariant.LUMO_TERTIARY);
+        createRelationButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderShowMetaInfoUI();
+            }
+        });
+
+        Button reloadRelationInfoButton= new Button("重新获取数据");
+        reloadRelationInfoButton.setIcon(VaadinIcon.REFRESH.create());
+        reloadRelationInfoButton.addThemeVariants(ButtonVariant.LUMO_ICON,ButtonVariant.LUMO_SMALL,ButtonVariant.LUMO_TERTIARY);
+        reloadRelationInfoButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderAddNewAttributeUI();
+            }
+        });
+
+        actionComponentsList.add(createRelationButton);
+        actionComponentsList.add(reloadRelationInfoButton);
+
+        Icon relationsIcon = VaadinIcon.RANDOM.create();
+        SecondaryTitleActionBar secondaryTitleActionBar = new SecondaryTitleActionBar(relationsIcon,"关联关系总量:",secondaryTitleComponentsList,actionComponentsList);
+        add(secondaryTitleActionBar);
     }
 }
