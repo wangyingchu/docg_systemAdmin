2da3d4f7c59c387358f82fcea8b40cfb64209cdb
wangyingchu <yingchuwang@gmail.com>
2022-09-22 18:05:12 +0800

    Modify ConceptionEntity Relation Graph

--- frontend/visualization/feature/conceptionEntitySpatialChart-connector.js
+++ frontend/visualization/feature/conceptionEntitySpatialChart-connector.js
@@ -6,16 +6,11 @@ window.Vaadin.Flow.feature_ConceptionEntitySpatialChart = {
         }
         c.$connector = {
             renderPointEntityContent : function(geoJsonStr) {
-                let reformattedGeoJsonStr = geoJsonStr.replaceAll('\\','');
-                if(reformattedGeoJsonStr.startsWith('"')){
-                    reformattedGeoJsonStr = reformattedGeoJsonStr.replace('"','');
-                }
-                if(reformattedGeoJsonStr.endsWith('"')){
-                    reformattedGeoJsonStr = reformattedGeoJsonStr.split("").reverse().join('').replace('"','').split('').reverse().join('');
-                }
-                const geoJsonObject = eval("(" + reformattedGeoJsonStr + ")");
-                },
+                c.$connector.renderEntityContent(geoJsonStr);
+            },
             renderPolygonEntityContent: function(geoJsonStr) {
+
+                /*
                 let reformattedGeoJsonStr = geoJsonStr.replaceAll('\\','');
                 if(reformattedGeoJsonStr.startsWith('"')){
                     reformattedGeoJsonStr = reformattedGeoJsonStr.replace('"','');
@@ -41,8 +36,16 @@ window.Vaadin.Flow.feature_ConceptionEntitySpatialChart = {
 
                     layer.bindPopup(popupContent);
                 }
-           },
+                */
+                c.$connector.renderEntityContent(geoJsonStr);
+
+            },
             renderLineEntityContent: function(geoJsonStr) {
+                c.$connector.renderEntityContent(geoJsonStr);
+            },
+
+
+            renderEntityContent: function(geoJsonStr) {
                 let reformattedGeoJsonStr = geoJsonStr.replaceAll('\\','');
                 if(reformattedGeoJsonStr.startsWith('"')){
                     reformattedGeoJsonStr = reformattedGeoJsonStr.replace('"','');
@@ -51,7 +54,97 @@ window.Vaadin.Flow.feature_ConceptionEntitySpatialChart = {
                     reformattedGeoJsonStr = reformattedGeoJsonStr.split("").reverse().join('').replace('"','').split('').reverse().join('');
                 }
                 const geoJsonObject = eval("(" + reformattedGeoJsonStr + ")");
+
+
+                var myStyle = {
+                    "color": "#ff7800",
+                    "weight": 5,
+                    "opacity": 0.65
+                };
+
+
+
+                L.geoJSON(geoJsonObject, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, {
+                            radius: 8,
+                            fillColor: '#749f83',
+                            color: "#000",
+                            weight: 1,
+                            opacity: 1,
+                            fillOpacity: 0.8
+                        });
+                    },
+                    style:myStyle,
+                    /*
+                    style:{weight: 1,
+                        color: "#000",
+                        opacity: 0.5,
+                        fillColor: '#749f83',
+                        fillOpacity: 0.8},
+
+                    style: function (feature) {
+
+                        console.log(feature);
+                        console.log(feature);
+                        console.log(feature);
+
+                        let geometryType = feature.geometry.type;
+                        if(geometryType == "Point"){}
+                        if(geometryType == "MultiPoint"){}
+                        if(geometryType == "LineString"){
+                            let lineStyle ={
+                                weight: 2,
+                                color: '#749f83',
+                                opacity: 1
+                            };
+                            return lineStyle;
+                        }
+                        if(geometryType == "MultiLineString"){
+                            let lineStyle ={
+                                weight: 2,
+                                color: '#749f83',
+                                opacity: 1
+                            };
+                            return lineStyle;
+                        }
+                        if(geometryType == "Polygon"){
+                            let areaStyle ={
+                                weight: 1,
+                                color: "#000",
+                                opacity: 0.5,
+                                fillColor: '#749f83',
+                                fillOpacity: 0.8
+                            };
+                            return areaStyle;
+                        }
+                        if(geometryType == "MultiPolygon"){
+                            let areaStyle ={
+                                weight: 1,
+                                color: "#000",
+                                opacity: 0.5,
+                                fillColor:  '#749f83',
+                                fillOpacity: 0.8
+                            };
+                            return areaStyle;
+                        }
+                    },
+
+*/
+                    onEachFeature: onEachFeature
+                }).addTo(map);
+
+                function onEachFeature(feature, layer) {
+                    let popupContent = '<p>I started out as a GeoJSON ' + feature.geometry.type + ', but now I\'m a Leaflet vector!</p>';
+                    if (feature.properties && feature.properties.popupContent) {
+                        popupContent += feature.properties.popupContent;
+                    }
+                    layer.bindPopup(popupContent);
+                }
+
             }
+
+
         };
         const mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
         const grayscale = L.tileLayer(mbUrl, {id: 'mapbox/light-v10'}), streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11'});
