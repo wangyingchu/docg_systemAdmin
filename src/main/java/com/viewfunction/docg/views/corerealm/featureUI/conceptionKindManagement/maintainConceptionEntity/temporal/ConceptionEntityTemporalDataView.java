17ad356cb17173b14d6f42c0f7a3b46e9f204874
wangyingchu <yingchuwang@gmail.com>
2022-10-09 16:24:53 +0800

    Modify ConceptionEntity Relation Graph

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/temporal/ConceptionEntityTemporalDataView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/temporal/ConceptionEntityTemporalDataView.java
@@ -2,25 +2,31 @@ package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement
 
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.accordion.Accordion;
+import com.vaadin.flow.component.accordion.AccordionPanel;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.TimeScaleDataPair;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeFlow;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeScaleEntity;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeScaleEvent;
 import com.viewfunction.docg.element.commonComponent.SecondaryIconTitle;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ConceptionEntityTemporalDataView extends VerticalLayout {
     private String conceptionKindName;
     private String conceptionEntityUID;
     private List<TimeScaleDataPair> timeScaleDataPairList;
+    private Accordion accordion;
 
     public ConceptionEntityTemporalDataView(){
         this.getStyle().set("padding-left","50px");
         SecondaryIconTitle secondaryIconTitle = new SecondaryIconTitle(VaadinIcon.LIST_SELECT.create(), "关联时间序列事件信息");
         add(secondaryIconTitle);
 
-        Accordion accordion = new Accordion();
+        accordion = new Accordion();
         accordion.setWidth(100,Unit.PERCENTAGE);
 
         Span name = new Span("Sophia Williams");
@@ -44,5 +50,39 @@ public class ConceptionEntityTemporalDataView extends VerticalLayout {
         this.conceptionKindName = conceptionKindName;
         this.conceptionEntityUID = conceptionEntityUID;
         this.timeScaleDataPairList = timeScaleDataPairList;
+
+        if(this.timeScaleDataPairList != null && this.timeScaleDataPairList.size() >0){
+            for(TimeScaleDataPair currentTimeScaleDataPair :this.timeScaleDataPairList){
+                TimeScaleEvent currentTimeScaleEvent = currentTimeScaleDataPair.getTimeScaleEvent();
+                TimeScaleEntity currentTimeScaleEntity = currentTimeScaleDataPair.getTimeScaleEntity();
+
+
+                LocalDateTime localDateTime = currentTimeScaleEvent.getReferTime();
+                currentTimeScaleEvent.getTimeScaleEventUID();
+                TimeFlow.TimeScaleGrade timeScaleGrade = currentTimeScaleEvent.getTimeScaleGrade();
+                currentTimeScaleEvent.getTimeFlowName();
+
+                String referTimeString = "";
+                switch(timeScaleGrade){
+                    case YEAR:
+                        referTimeString = ""+localDateTime.getYear();
+                        break;
+                    case MONTH:
+                        referTimeString = ""+localDateTime.getYear()+"-"+localDateTime.getMonth().getValue();
+                        break;
+                    case DAY:
+                        referTimeString = ""+localDateTime.getYear()+"-"+localDateTime.getMonthValue()+"-"+localDateTime.getDayOfMonth();
+                        break;
+                    case HOUR:break;
+                    case MINUTE:break;
+                    case SECOND:break;
+                }
+
+                String eventTitle = currentTimeScaleEvent.getEventComment()+" "+referTimeString;
+                SecondaryIconTitle secondaryIconTitle = new SecondaryIconTitle(VaadinIcon.LIST_SELECT.create(), eventTitle);
+                AccordionPanel accordionPanel = new AccordionPanel(secondaryIconTitle,new VerticalLayout());
+                accordion.add(accordionPanel);
+            }
+        }
     }
 }
