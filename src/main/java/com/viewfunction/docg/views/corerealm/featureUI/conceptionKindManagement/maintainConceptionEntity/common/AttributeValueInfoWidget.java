ededc75e1622161c44a439d356424b8a715e3891
wangyingchu <yingchuwang@gmail.com>
2022-10-21 17:27:49 +0800

    Update for TemporalEventDetailWidget feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/common/AttributeValueInfoWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/common/AttributeValueInfoWidget.java
@@ -1,6 +1,70 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity.common;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeDataType;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
+import dev.mett.vaadin.tooltip.Tooltips;
 
 public class AttributeValueInfoWidget extends HorizontalLayout {
+
+    public AttributeValueInfoWidget(AttributeValue attributeValue){
+        String attributeName = attributeValue.getAttributeName();
+        Object attributeNameValue = attributeValue.getAttributeValue();
+        AttributeDataType attributeDataType = attributeValue.getAttributeDataType();
+
+        Icon propertyTypeIcon = null;
+        if(attributeName.startsWith(RealmConstant.RealmInnerTypePerFix) ||
+                attributeName.equals(RealmConstant._createDateProperty) ||
+                attributeName.equals(RealmConstant._lastModifyDateProperty) ||
+                attributeName.equals(RealmConstant._creatorIdProperty)||
+                attributeName.equals(RealmConstant._dataOriginProperty)
+        ){
+            propertyTypeIcon = VaadinIcon.ELLIPSIS_CIRCLE_O.create();
+        }else{
+            propertyTypeIcon = VaadinIcon.ELLIPSIS_CIRCLE.create();
+        }
+
+        propertyTypeIcon.setSize("12px");
+        add(propertyTypeIcon);
+        setVerticalComponentAlignment(Alignment.CENTER,propertyTypeIcon);
+
+        Label attributeNameLabel = new Label(attributeName);
+        attributeNameLabel.getStyle().set("font-size","0.75rem").set("font-weight","bold");
+        add(attributeNameLabel);
+        setVerticalComponentAlignment(Alignment.CENTER,attributeNameLabel);
+
+        Label attributeTypeLabel = new Label(attributeDataType.toString());
+        attributeTypeLabel.addClassNames("text-tertiary");
+        attributeTypeLabel.getStyle().set("font-size","0.7rem").set("color","var(--lumo-contrast-70pct)").set("padding-right","8px");
+        add(attributeTypeLabel);
+        setVerticalComponentAlignment(Alignment.CENTER,attributeTypeLabel);
+
+        Label attributeValueLabel = new Label(attributeNameValue.toString());
+        attributeValueLabel.getStyle().set("font-size","var(--lumo-font-size-m)")
+                .set("color","#2e4e7e").set("font-weight","bold");
+        add(attributeValueLabel);
+        setVerticalComponentAlignment(Alignment.CENTER,attributeValueLabel);
+
+        Button showAttributeKindInfoButton = new Button("属性类型详情");
+        showAttributeKindInfoButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY,ButtonVariant.LUMO_SMALL);
+        showAttributeKindInfoButton.getStyle().set("font-size","12px");
+        showAttributeKindInfoButton.setIcon(VaadinIcon.EYE.create());
+        Tooltips.getCurrent().setTooltip(showAttributeKindInfoButton, "显示属性类型详情");
+        showAttributeKindInfoButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+
+            }
+        });
+        add(showAttributeKindInfoButton);
+        setVerticalComponentAlignment(Alignment.CENTER,showAttributeKindInfoButton);
+    }
 }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/temporal/TemporalEventDetailWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/temporal/TemporalEventDetailWidget.java
@@ -1,9 +1,7 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity.temporal;
 
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.Classification;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeScaleEntity;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeScaleEvent;
 import com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity.common.AttributeValueInfoWidget;
@@ -21,18 +19,9 @@ public class TemporalEventDetailWidget extends VerticalLayout {
         List<AttributeValue> attributeValueList = timeScaleEvent.getAttributes();
         if(attributeValueList != null){
             for(AttributeValue currentAttributeValue:attributeValueList){
-                currentAttributeValue.getAttributeName();
-                currentAttributeValue.getAttributeValue();
-                currentAttributeValue.getAttributeDataType();
-                Label label = new Label(currentAttributeValue.getAttributeName()+":"+currentAttributeValue.getAttributeValue());
-                add(label);
-                AttributeValueInfoWidget attributeValueInfoWidget = new AttributeValueInfoWidget();
+                AttributeValueInfoWidget attributeValueInfoWidget = new AttributeValueInfoWidget(currentAttributeValue);
                 add(attributeValueInfoWidget);
             }
         }
-
-
-
-        //List<Classification> classificationList = timeScaleEvent.getAttachedClassifications(null,null);
     }
 }
