6c3384a093d37200e0a01e109a65a8fd7c68c849
wangyingchu <yingchuwang@gmail.com>
2023-01-18 16:40:32 +0800

    update for ConceptionEntityTemporalSunburstChart

--- frontend/visualization/feature/conceptionEntityTemporalSunburstChart-connector.js
+++ frontend/visualization/feature/conceptionEntityTemporalSunburstChart-connector.js
@@ -12,57 +12,57 @@ window.Vaadin.Flow.feature_ConceptionEntityTemporalSunburstChart = {
                     children: [{
                         name: "2021",
                         children: [
-                            { name: "2021-1", value: 100 },
-                            { name: "A2", value: 60 }
+                            { name: "2021-1", value: 1 },
+                            { name: "A2", value: 1 }
                         ]
                     },
                         { name: "Second",
                             children: [
-                                { name: "B1", value: 135 },
-                                { name: "B2", value: 98 }
+                                { name: "B1", value: 1 },
+                                { name: "B2", value: 1 }
                             ]
                         },
                         { name: "Third",
                             children: [
                                 { name: "C1",
                                     children: [
-                                        { name: "EE1", value: 130 },
-                                        { name: "EE2", value: 87 },
-                                        { name: "EE3", value: 55 }
+                                        { name: "EE1", value: 1 },
+                                        { name: "EE2", value: 1 },
+                                        { name: "EE3", value: 1 }
                                     ]
                                 },
-                                { name: "C2", value: 148 },
+                                { name: "C2", value: 1 },
                                 { name: "C3", children: [
-                                        { name: "CC1", value: 53 },
-                                        { name: "CC2", value: 30 }
+                                        { name: "CC1", value: 1 },
+                                        { name: "CC2", value: 1 }
                                     ]
                                 },
-                                { name: "C4", value: 26 }
+                                { name: "C4", value: 1 }
                             ]
                         },
                         { name: "Fourth",
                             children: [
-                                { name: "D1", value: 415 },
-                                { name: "D2", value: 148 },
-                                { name: "D3", value: 89 }
+                                { name: "D1", value: 1 },
+                                { name: "D2", value: 1 },
+                                { name: "D3", value: 1 }
                             ]
                         },
                         { name: "Fifth",
                             children: [
                                 { name: "E1",
                                     children: [
-                                        { name: "EE1", value: 33 },
-                                        { name: "EE2", value: 40 },
-                                        { name: "EE3", value: 89 }
+                                        { name: "EE1", value: 1 },
+                                        { name: "EE2", value: 1 },
+                                        { name: "EE3", value: 1 }
                                     ]
                                 },
-                                { name: "E2",value: 148 }
+                                { name: "E2",value: 1 }
                             ]
                         }]
                 }];
 
-                //c.$connector.sunburstChart.data.setAll(data);
-                c.$connector.sunburstChart.data.setAll([sunburstEntitiesObject]);
+                c.$connector.sunburstChart.data.setAll(data);
+                //c.$connector.sunburstChart.data.setAll([sunburstEntitiesObject]);
 
                 let legend = c.$connector.container.children.push(am5.Legend.new(c.$connector.root, {
                     centerX: am5.percent(50),
