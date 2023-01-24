37ebd65efb13df6cc6e19219bce5cc4bbacbc9b8
wangyingchu <yingchuwang@gmail.com>
2023-01-24 08:12:45 +0800

    update for Common Pie Chart

---
+++ frontend/visualization/common/pieChart_echarts-connector.js
@@ -0,0 +1,12 @@
+window.Vaadin.Flow.common_PieChart_echarts = {
+    initLazy: function (c) {
+        // Check whether the connector was already initialized
+        if (c.$connector) {
+            return;
+        }
+        c.$connector = {
+            // functions
+        };
+
+    }
+}
