1b1cd2dcb9334f9a9c8c00363b2e73ec30dceb4f
wangyingchu <yingchuwang@gmail.com>
2023-02-02 23:50:58 +0800

    update for RelationAndConceptionKindAttachInfoWidget

---
+++ src/main/java/com/viewfunction/docg/element/commonComponent/chart/CartesianHeatmapChart.java
@@ -0,0 +1,31 @@
+package com.viewfunction.docg.element.commonComponent.chart;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.function.SerializableConsumer;
+
+@JavaScript("./visualization/common/cartesianHeatmapChart_echarts-connector.js")
+public class CartesianHeatmapChart extends Div {
+
+    public CartesianHeatmapChart(){
+        UI.getCurrent().getPage().addJavaScript("js/echarts/5.4.1/dist/echarts.min.js");
+        UI.getCurrent().getPage().addJavaScript("js/echarts/5.4.1/dist/extension/dataTool.min.js");
+        UI.getCurrent().getPage().addJavaScript("js/echarts/5.4.1/dist/extension/bmap.min.js");
+        setWidth(400, Unit.PIXELS);
+        setHeight(500,Unit.PIXELS);
+        initConnector(getElement());
+    }
+
+    private void initConnector(Element layout) {
+        runBeforeClientResponse(ui -> ui.getPage().executeJs(
+                "window.Vaadin.Flow.common_CartesianHeatmapChart_echarts.initLazy($0)", getElement()));
+    }
+
+    private void runBeforeClientResponse(SerializableConsumer<UI> command) {
+        getElement().getNode().runWhenAttached(ui -> ui
+                .beforeClientResponse(this, context -> command.accept(ui)));
+    }
+}
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/RelationAndConceptionKindAttachInfoWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/RelationAndConceptionKindAttachInfoWidget.java
@@ -1,11 +1,17 @@
 package com.viewfunction.docg.views.corerealm.featureUI.coreRealmData;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.viewfunction.docg.element.commonComponent.chart.CartesianHeatmapChart;
 
 public class RelationAndConceptionKindAttachInfoWidget extends VerticalLayout {
 
     public RelationAndConceptionKindAttachInfoWidget(){
-        this.getStyle().set("background-color","#EFEFEF");
+        //this.getStyle().set("background-color","#EFEFEF");
+        CartesianHeatmapChart cartesianHeatmapChart1 = new CartesianHeatmapChart();
+        add(cartesianHeatmapChart1);
+
+        CartesianHeatmapChart cartesianHeatmapChart2 = new CartesianHeatmapChart();
+        add(cartesianHeatmapChart2);
     }
 
     public void refreshRelationAndConceptionKindAttachInfo(){}
