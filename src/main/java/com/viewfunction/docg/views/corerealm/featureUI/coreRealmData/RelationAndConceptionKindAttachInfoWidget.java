5bfa7d710b5aabc4abec932612df578bc5eed321
wangyingchu <yingchuwang@gmail.com>
2023-02-06 14:17:43 +0800

    update for RelationAndConceptionKindAttachInfoWidget feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/RelationAndConceptionKindAttachInfoWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/RelationAndConceptionKindAttachInfoWidget.java
@@ -18,12 +18,21 @@ import java.util.List;
 public class RelationAndConceptionKindAttachInfoWidget extends VerticalLayout {
 
     public RelationAndConceptionKindAttachInfoWidget(){
+
+
+        String[] conceptionKindsLabel_x = new String[]{"ABCDEF", "1avdvdvd", "2avdvddd", "3avddddd", "4a", "5a", "6a",
+                "7a"," '8a'", "9a","10a","11a","12p", "1p", "2p", "3p", "4p", "5p","6p", "7p", "8p", "9p", "10p", "GHIJK"};
+        String[] relationKindsLabel_y = new String[]{"A_Saturdayvddvdd", "Fridayvdvddd", "Thursdayvdvdvd",
+                "Wednesdaydvdvddd", "Tuesdayvdvdd", "Mondayvdvdd", "SundayB_"};
+
         ThirdLevelIconTitle infoTitle1 = new ThirdLevelIconTitle(new Icon(VaadinIcon.COMPRESS_SQUARE),"概念与关系实体入度统计");
         add(infoTitle1);
         CartesianHeatmapChart cartesianHeatmapChart1 = new CartesianHeatmapChart(380,280);
         cartesianHeatmapChart1.setColorRange("WhiteSmoke","#168eea");
         cartesianHeatmapChart1.setName("领域概念与关系实体入度统计");
         cartesianHeatmapChart1.hideLabels();
+        cartesianHeatmapChart1.setXAxisLabel(conceptionKindsLabel_x);
+        cartesianHeatmapChart1.setYAxisLabel(relationKindsLabel_y);
         cartesianHeatmapChart1.setData();
         add(cartesianHeatmapChart1);
 
