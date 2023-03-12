30b48081a540c665be3b10565086f9297ed7e490
wangyingchu <yingchuwang@gmail.com>
2023-03-12 23:34:54 +0800

    RelationEntity Query feature

---
+++ src/main/java/com/viewfunction/docg/util/RelationEntityResourceHolderVO.java
@@ -0,0 +1,32 @@
+package com.viewfunction.docg.util;
+
+public class RelationEntityResourceHolderVO {
+
+    private String relationKind;
+    private String relationEntityUID;
+    private String comment;
+
+    public String getRelationKind() {
+        return relationKind;
+    }
+
+    public void setRelationKind(String relationKind) {
+        this.relationKind = relationKind;
+    }
+
+    public String getRelationEntityUID() {
+        return relationEntityUID;
+    }
+
+    public void setRelationEntityUID(String relationEntityUID) {
+        this.relationEntityUID = relationEntityUID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
--- src/main/java/com/viewfunction/docg/util/ResourceHolder.java
+++ src/main/java/com/viewfunction/docg/util/ResourceHolder.java
@@ -19,8 +19,14 @@ public class ResourceHolder {
 
     private static List<ConceptionEntityResourceHolderVO> conceptionEntityProcessingDataList = new ArrayList<>();
 
+    private static List<RelationEntityResourceHolderVO> relationEntityProcessingDataList = new ArrayList<>();
+
     public static List<ConceptionEntityResourceHolderVO> getConceptionEntityProcessingDataList(){
         return conceptionEntityProcessingDataList;
+    }
+
+    public static List<RelationEntityResourceHolderVO> getRelationEntityProcessingDataList(){
+        return relationEntityProcessingDataList;
     }
 
     public static boolean addConceptionEntityToProcessingList(ConceptionEntityResourceHolderVO conceptionEntityResourceHolderVO){
@@ -38,12 +44,38 @@ public class ResourceHolder {
         return true;
     }
 
+    public static boolean addRelationEntityToProcessingList(RelationEntityResourceHolderVO relationEntityResourceHolderVO){
+        String relationEntityUID = relationEntityResourceHolderVO.getRelationEntityUID();
+        boolean alreadyInList = false;
+        for(RelationEntityResourceHolderVO currentRelationEntityResourceHolderVO:relationEntityProcessingDataList){
+            if(relationEntityUID.equals(currentRelationEntityResourceHolderVO.getRelationEntityUID())){
+                alreadyInList = true;
+                break;
+            }
+        }
+        if(!alreadyInList){
+            relationEntityProcessingDataList.add(relationEntityResourceHolderVO);
+        }
+        return true;
+    }
+
     public static boolean removeConceptionEntityFromProcessingList(String conceptionEntityUID){
         boolean removeSuccessFlag = false;
         for(ConceptionEntityResourceHolderVO currentConceptionEntityResourceHolderVO:conceptionEntityProcessingDataList){
             if(conceptionEntityUID.equals(currentConceptionEntityResourceHolderVO.getConceptionEntityUID())){
                 conceptionEntityProcessingDataList.remove(currentConceptionEntityResourceHolderVO);
+                removeSuccessFlag = true;
+                break;
+            }
+        }
+        return removeSuccessFlag;
+    }
 
+    public static boolean removeRelationEntityFromProcessingList(String relationEntityUID){
+        boolean removeSuccessFlag = false;
+        for(RelationEntityResourceHolderVO currentRelationEntityResourceHolderVO:relationEntityProcessingDataList){
+            if(relationEntityUID.equals(currentRelationEntityResourceHolderVO.getRelationEntityUID())){
+                relationEntityProcessingDataList.remove(currentRelationEntityResourceHolderVO);
                 removeSuccessFlag = true;
                 break;
             }
@@ -53,5 +85,9 @@ public class ResourceHolder {
 
     public static void clearConceptionEntityProcessingList(){
         conceptionEntityProcessingDataList.clear();
+    }
+
+    public static void clearRelationEntityProcessingList(){
+        relationEntityProcessingDataList.clear();
     }
 }
