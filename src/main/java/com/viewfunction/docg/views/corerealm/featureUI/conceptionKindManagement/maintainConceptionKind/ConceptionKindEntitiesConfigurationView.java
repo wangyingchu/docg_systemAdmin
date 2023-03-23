a90f8c0478617a1936c50c3d611dcbacb1caed73
wangyingchu <yingchuwang@gmail.com>
2023-03-24 00:06:33 +0800

    Update for ConceptionKindDetailView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/ConceptionKindEntitiesConfigurationView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/ConceptionKindEntitiesConfigurationView.java
@@ -3,6 +3,9 @@ package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement
 import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import com.viewfunction.docg.element.commonComponent.PrimaryKeyValueDisplayItem;
 
 import java.text.NumberFormat;
@@ -15,10 +18,19 @@ public class ConceptionKindEntitiesConfigurationView extends VerticalLayout {
         this.conceptionKindName = conceptionKindName;
 
         HorizontalLayout infoContainer = new HorizontalLayout();
+        infoContainer.setDefaultVerticalComponentAlignment(Alignment.CENTER);
         add(infoContainer);
 
+        CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+        long conceptionEntitiesCount = 0;
+        try {
+            conceptionEntitiesCount = coreRealm.getConceptionKind(this.conceptionKindName).countConceptionEntities();
+        } catch (CoreRealmServiceRuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
         NumberFormat numberFormat = NumberFormat.getInstance();
-        new PrimaryKeyValueDisplayItem(infoContainer, FontAwesome.Solid.CIRCLE.create(),"概念实体数量:",numberFormat.format(23849054));
+        new PrimaryKeyValueDisplayItem(infoContainer, FontAwesome.Solid.CIRCLE.create(),"概念实体数量:",numberFormat.format(conceptionEntitiesCount));
 
     }
 }
