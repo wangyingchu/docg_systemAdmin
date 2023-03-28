5959e6dcbfef15878d780043b840863dcba3003c
wangyingchu <yingchuwang@gmail.com>
2023-03-28 17:55:40 +0800

    Update for ConceptionKindDetailView feature

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/metaConfigItem/MetaConfigItemsConfigView.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.metaConfigItem;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class MetaConfigItemsConfigView extends VerticalLayout {
+}
