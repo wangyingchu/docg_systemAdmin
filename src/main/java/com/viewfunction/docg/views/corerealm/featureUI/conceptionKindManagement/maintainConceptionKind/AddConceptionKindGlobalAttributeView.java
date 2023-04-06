3fa7ca48a7d64bc823a3575aafa18842ef79cbb7
wangyingchu <yingchuwang@gmail.com>
2023-04-06 21:10:38 +0800

    Update for AddConceptionKindGlobalAttributeView feature

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/AddConceptionKindGlobalAttributeView.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionKind;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class AddConceptionKindGlobalAttributeView extends VerticalLayout {
+}
