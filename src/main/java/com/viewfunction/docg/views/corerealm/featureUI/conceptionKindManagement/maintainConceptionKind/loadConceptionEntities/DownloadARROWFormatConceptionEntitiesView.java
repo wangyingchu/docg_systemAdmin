9d64bcdfc57ae5d69b471c7187a6700f0bc49752
wangyingchu <yingchuwang@gmail.com>
2023-04-23 10:57:55 +0800

    Update for DownloadARROWFormatConceptionEntitiesView feature

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/loadConceptionEntities/DownloadARROWFormatConceptionEntitiesView.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionKind.loadConceptionEntities;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class DownloadARROWFormatConceptionEntitiesView extends VerticalLayout {
+}
