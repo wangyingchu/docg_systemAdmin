7332858faa0fa7fdb0b78cae31678155973fc7d0
wangyingchu <yingchuwang@gmail.com>
2023-05-14 22:57:04 +0800

    Update for ClassificationManagementUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/ClassificationManagementUI.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/ClassificationManagementUI.java
@@ -1,11 +1,99 @@
 package com.viewfunction.docg.views.corerealm.featureUI;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.viewfunction.docg.element.commonComponent.SectionActionBar;
+import com.viewfunction.docg.element.commonComponent.TitleActionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassificationManagementUI extends VerticalLayout {
 
     public ClassificationManagementUI(){
-        add(new Label("ClassificationManagementUI"));
+
+
+        Button refreshDataButton = new Button("刷新分类数据统计信息",new Icon(VaadinIcon.REFRESH));
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
+        refreshDataButton.addClickListener((ClickEvent<Button> click) ->{
+            //loadConceptionKindsInfo();
+            //resetSingleConceptionKindSummaryInfoArea();
+        });
+
+        List<Component> buttonList = new ArrayList<>();
+        buttonList.add(refreshDataButton);
+
+        List<Component> secTitleElementsList = new ArrayList<>();
+
+        Icon realmIcon = VaadinIcon.ARCHIVE.create();
+        realmIcon.getStyle().set("padding", "var(--lumo-space-xs");
+        Span realmNameSpan = new Span( realmIcon,new Span("Default CoreRealm"));
+        realmNameSpan.addClassName("text-2xs");
+        realmNameSpan.getElement().getThemeList().add("badge contrast");
+        secTitleElementsList.add(realmNameSpan);
+
+        TitleActionBar titleActionBar = new TitleActionBar(new Icon(VaadinIcon.COG_O),"Classification 分类数据管理",secTitleElementsList,buttonList);
+        add(titleActionBar);
+
+
+
+        List<Component> conceptionKindManagementOperationButtonList = new ArrayList<>();
+
+        Button conceptionKindRelationGuideButton = new Button("概念实体关联分布概览",new Icon(VaadinIcon.DASHBOARD));
+        conceptionKindRelationGuideButton.setDisableOnClick(true);
+        conceptionKindRelationGuideButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        conceptionKindRelationGuideButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        //conceptionKindManagementOperationButtonList.add(conceptionKindRelationGuideButton);
+        conceptionKindRelationGuideButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderConceptionKindsCorrelationInfoSummaryUI(conceptionKindRelationGuideButton);
+            }
+        });
+
+        Button processingDataListButton = new Button("待处理数据",new Icon(VaadinIcon.MAILBOX));
+        processingDataListButton.setDisableOnClick(true);
+        processingDataListButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        processingDataListButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        //conceptionKindManagementOperationButtonList.add(processingDataListButton);
+        processingDataListButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderProcessingDataListUI(processingDataListButton);
+            }
+        });
+
+        Button createConceptionKindButton = new Button("创建根分类",new Icon(VaadinIcon.PLUS_SQUARE_O));
+        createConceptionKindButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        createConceptionKindButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        conceptionKindManagementOperationButtonList.add(createConceptionKindButton);
+        createConceptionKindButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderCreateConceptionKindUI();
+            }
+        });
+
+        Icon icon = new Icon(VaadinIcon.LIST);
+        SectionActionBar sectionActionBar = new SectionActionBar(icon,"分类数据:",conceptionKindManagementOperationButtonList);
+        add(sectionActionBar);
+
+
+
+
+
+
+
     }
 }
