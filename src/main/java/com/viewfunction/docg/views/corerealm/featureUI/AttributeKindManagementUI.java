cb331dbbd4d3e89e7612ace9a0cbacc53b804d82
wangyingchu <yingchuwang@gmail.com>
2023-05-20 10:14:39 +0800

    Update for AttributesViewKindManagementUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/AttributeKindManagementUI.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/AttributeKindManagementUI.java
@@ -1,11 +1,46 @@
 package com.viewfunction.docg.views.corerealm.featureUI;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.viewfunction.docg.element.commonComponent.TitleActionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AttributeKindManagementUI extends VerticalLayout {
 
     public AttributeKindManagementUI(){
-        add(new Label("AttributeKindManagementUI"));
+
+        Button refreshDataButton = new Button("刷新属性类型数据统计信息",new Icon(VaadinIcon.REFRESH));
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
+        refreshDataButton.addClickListener((ClickEvent<Button> click) ->{
+            //loadConceptionKindsInfo();
+            //resetSingleConceptionKindSummaryInfoArea();
+        });
+
+        List<Component> buttonList = new ArrayList<>();
+        buttonList.add(refreshDataButton);
+
+        List<Component> secTitleElementsList = new ArrayList<>();
+
+        Icon realmIcon = VaadinIcon.ARCHIVE.create();
+        realmIcon.getStyle().set("padding", "var(--lumo-space-xs");
+        Span realmNameSpan = new Span( realmIcon,new Span("Default CoreRealm"));
+        realmNameSpan.addClassName("text-2xs");
+        realmNameSpan.getElement().getThemeList().add("badge contrast");
+        secTitleElementsList.add(realmNameSpan);
+
+        TitleActionBar titleActionBar = new TitleActionBar(new Icon(VaadinIcon.COG_O),"Attribute Kind 属性类型数据管理",secTitleElementsList,buttonList);
+        add(titleActionBar);
     }
 }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/AttributesViewKindManagementUI.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/AttributesViewKindManagementUI.java
@@ -1,11 +1,45 @@
 package com.viewfunction.docg.views.corerealm.featureUI;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.viewfunction.docg.element.commonComponent.TitleActionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AttributesViewKindManagementUI extends VerticalLayout {
 
     public AttributesViewKindManagementUI(){
-        add(new Label("AttributesViewKindManagementUI"));
+        Button refreshDataButton = new Button("刷新属性视图类型数据统计信息",new Icon(VaadinIcon.REFRESH));
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        refreshDataButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
+        refreshDataButton.addClickListener((ClickEvent<Button> click) ->{
+            //loadConceptionKindsInfo();
+            //resetSingleConceptionKindSummaryInfoArea();
+        });
+
+        List<Component> buttonList = new ArrayList<>();
+        buttonList.add(refreshDataButton);
+
+        List<Component> secTitleElementsList = new ArrayList<>();
+
+        Icon realmIcon = VaadinIcon.ARCHIVE.create();
+        realmIcon.getStyle().set("padding", "var(--lumo-space-xs");
+        Span realmNameSpan = new Span( realmIcon,new Span("Default CoreRealm"));
+        realmNameSpan.addClassName("text-2xs");
+        realmNameSpan.getElement().getThemeList().add("badge contrast");
+        secTitleElementsList.add(realmNameSpan);
+
+        TitleActionBar titleActionBar = new TitleActionBar(new Icon(VaadinIcon.COG_O),"Attributes View Kind 属性视图类型数据管理",secTitleElementsList,buttonList);
+        add(titleActionBar);
     }
 }
