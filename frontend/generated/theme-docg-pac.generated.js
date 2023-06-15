98edf33f9f1660cce2e248af860a8107800f3e15
wangyingchu <yingchuwang@gmail.com>
2023-06-15 09:21:15 +0800

    Update vaadin to 24.1.0

--- frontend/generated/theme-docg-pac.generated.js
+++ frontend/generated/theme-docg-pac.generated.js
@@ -1,136 +1,59 @@
 import 'construct-style-sheets-polyfill';
-import { unsafeCSS, registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles';
-import stripCssComments from 'strip-css-comments';
-
-const createLinkReferences = (css, target) => {
-  // Unresolved urls are written as '@import url(text);' or '@import "text";' to the css
-  // media query can be present on @media tag or on @import directive after url
-  // Note that with Vite production build there is no space between @import and "text"
-  // [0] is the full match
-  // [1] matches the media query
-  // [2] matches the url
-  // [3] matches the quote char surrounding in '@import "..."'
-  // [4] matches the url in '@import "..."'
-  // [5] matches media query on @import statement
-  const importMatcher = /(?:@media\s(.+?))?(?:\s{)?\@import\s*(?:url\(\s*['"]?(.+?)['"]?\s*\)|(["'])((?:\\.|[^\\])*?)\3)([^;]*);(?:})?/g
-  
-  // Only cleanup if comment exist
-  if(/\/\*(.|[\r\n])*?\*\//gm.exec(css) != null) {
-    // clean up comments
-    css = stripCssComments(css);
-  }
-  
-  var match;
-  var styleCss = css;
-  
-  // For each external url import add a link reference
-  while((match = importMatcher.exec(css)) !== null) {
-    styleCss = styleCss.replace(match[0], "");
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = match[2] || match[4];
-    const media = match[1] || match[5];
-    if (media) {
-      link.media = media;
-    }
-    // For target document append to head else append to target
-    if (target === document) {
-      document.head.appendChild(link);
-    } else {
-      target.appendChild(link);
-    }
-  };
-  return styleCss;
-};
-
-// target: Document | ShadowRoot
-export const injectGlobalCss = (css, target, first) => {
-  if(target === document) {
-    const hash = getHash(css);
-    if (window.Vaadin.theme.injectedGlobalCss.indexOf(hash) !== -1) {
-      return;
-    }
-    window.Vaadin.theme.injectedGlobalCss.push(hash);
-  }
-  const sheet = new CSSStyleSheet();
-  sheet.replaceSync(createLinkReferences(css,target));
-  if (first) {
-    target.adoptedStyleSheets = [sheet, ...target.adoptedStyleSheets];
-  } else {
-    target.adoptedStyleSheets = [...target.adoptedStyleSheets, sheet];
-  }
-};
-
-const addLumoImportStyleTag = (lumoStyles, target) => {
-  const styleTag = document.createElement('style');
-  styleTag.type = 'text/css';
-  styleTag.appendChild(document.createTextNode(lumoStyles));
-  // For target document append to head else append to target
-  if (target === document) {
-    document.head.appendChild(styleTag);
-  } else {
-    target.appendChild(styleTag);
-  }
-};
+import { injectGlobalCss } from 'Frontend/generated/jar-resources/theme-util.js';
+import './theme-docg-pac.components.generated.js';
+let needsReloadOnChanges = false;
 import { typography } from '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/typography-global.js';
 import { color } from '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/color-global.js';
 import { spacing } from '@vaadin/vaadin-lumo-styles/spacing.js';
 import { badge } from '@vaadin/vaadin-lumo-styles/badge.js';
+import '@vaadin/vaadin-lumo-styles/badge-global.js';
 import { utility } from '@vaadin/vaadin-lumo-styles/utility.js';
-import vaadinGridCss from 'themes/docg-pac/components/vaadin-grid.css?inline';
+import '@vaadin/vaadin-lumo-styles/utility-global.js';
 
-window.Vaadin = window.Vaadin || {};
-window.Vaadin.theme = window.Vaadin.theme || {};
-window.Vaadin.theme.injectedGlobalCss = [];
+  let themeRemovers = new WeakMap();
+  let targets = [];
 
-/**
- * Calculate a 32 bit FNV-1a hash
- * Found here: https://gist.github.com/vaiorabbit/5657561
- * Ref.: http://isthe.com/chongo/tech/comp/fnv/
- *
- * @param {string} str the input value
- * @returns {string} 32 bit (as 8 byte hex string)
- */
-function hashFnv32a(str) {
-  /*jshint bitwise:false */
-  let i, l, hval = 0x811c9dc5;
+  export const applyTheme = (target) => {
+    const removers = [];
+    if (target !== document) {
+      removers.push(injectGlobalCss(typography.cssText, '', target, true));
+removers.push(injectGlobalCss(color.cssText, '', target, true));
+removers.push(injectGlobalCss(spacing.cssText, '', target, true));
+removers.push(injectGlobalCss(badge.cssText, '', target, true));
+removers.push(injectGlobalCss(utility.cssText, '', target, true));
 
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    
+    
+
+    if (import.meta.hot) {
+      targets.push(new WeakRef(target));
+      themeRemovers.set(target, removers);
+    }
+
   }
+  
 
-  // Convert to 8 digit hex string
-  return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+if (import.meta.hot) {
+  import.meta.hot.accept((module) => {
+
+    if (needsReloadOnChanges) {
+      window.location.reload();
+    } else {
+      targets.forEach(targetRef => {
+        const target = targetRef.deref();
+        if (target) {
+          themeRemovers.get(target).forEach(remover => remover())
+          module.applyTheme(target);
+        }
+      })
+    }
+  });
+
+  import.meta.hot.on('vite:afterUpdate', (update) => {
+    document.dispatchEvent(new CustomEvent('vaadin-theme-updated', { detail: update }));
+  });
 }
 
-/**
- * Calculate a 64 bit hash for the given input.
- * Double hash is used to significantly lower the collision probability.
- *
- * @param {string} input value to get hash for
- * @returns {string} 64 bit (as 16 byte hex string)
- */
-function getHash(input) {
-  let h1 = hashFnv32a(input); // returns 32 bit (as 8 byte hex string)
-  return h1 + hashFnv32a(h1 + input); 
-}
-export const applyTheme = (target) => {
-  
-  
-  
-  if (!document['_vaadintheme_docg-pac_componentCss']) {
-    registerStyles(
-        'vaadin-grid',
-        unsafeCSS(vaadinGridCss.toString())
-      );
-      
-    document['_vaadintheme_docg-pac_componentCss'] = true;
-  }
-  addLumoImportStyleTag(typography.cssText, target);
-addLumoImportStyleTag(color.cssText, target);
-addLumoImportStyleTag(spacing.cssText, target);
-addLumoImportStyleTag(badge.cssText, target);
-addLumoImportStyleTag(utility.cssText, target);
-
-}
--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -1,8 +1,9 @@
-import './vaadin-featureflags.ts';
+import './vaadin-featureflags.js';
 
 import './index';
 
 import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
 
+import './theme-docg-pac.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
