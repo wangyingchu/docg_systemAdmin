533b7ca2847be0fd52bab16f1d724441f808df6f
wangyingchu <yingchuwang@gmail.com>
2023-06-20 09:21:32 +0800

    Update for RelationKindDetailUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/relationKindManagement/maintainRelationKind/RelationKindDetailUI.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/relationKindManagement/maintainRelationKind/RelationKindDetailUI.java
@@ -3,19 +3,29 @@ package com.viewfunction.docg.views.corerealm.featureUI.relationKindManagement.m
 import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.shared.Registration;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.EntityStatisticsInfo;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.KindEntityAttributeRuntimeStatistics;
+import com.viewfunction.docg.element.commonComponent.LightGridColumnHeader;
 import com.viewfunction.docg.element.commonComponent.SecondaryTitleActionBar;
+import com.viewfunction.docg.element.commonComponent.ThirdLevelIconTitle;
 import com.viewfunction.docg.element.eventHandling.ConceptionKindConfigurationInfoRefreshEvent;
 import com.viewfunction.docg.util.ResourceHolder;
 import com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.kindMaintain.KindDescriptionEditorItemWidget;
+import dev.mett.vaadin.tooltip.Tooltips;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +39,9 @@ public class RelationKindDetailUI extends VerticalLayout implements
     private int relationKindDetailViewHeightOffset = 110;
     private Registration listener;
     private int currentBrowserHeight = 0;
+    private VerticalLayout leftSideContainerLayout;
+    private VerticalLayout rightSideContainerLayout;
+    private Grid<KindEntityAttributeRuntimeStatistics> conceptionKindAttributesInfoGrid;
 
     public RelationKindDetailUI(){}
 
@@ -127,6 +140,78 @@ public class RelationKindDetailUI extends VerticalLayout implements
         HorizontalLayout mainContainerLayout = new HorizontalLayout();
         mainContainerLayout.setWidthFull();
         add(mainContainerLayout);
+
+        leftSideContainerLayout = new VerticalLayout();
+        leftSideContainerLayout.setSpacing(false);
+        leftSideContainerLayout.setPadding(false);
+        leftSideContainerLayout.setMargin(false);
+
+        mainContainerLayout.add(leftSideContainerLayout);
+        leftSideContainerLayout.setWidth(800, Unit.PIXELS);
+        leftSideContainerLayout.getStyle()
+                .set("border-right", "1px solid var(--lumo-contrast-20pct)");
+
+        ThirdLevelIconTitle infoTitle1 = new ThirdLevelIconTitle(new Icon(VaadinIcon.ALIGN_LEFT),"关系类型属性分布 (实体概略采样数 "+10000+")");
+        infoTitle1.getStyle().set("padding-bottom","5px");
+
+        leftSideContainerLayout.add(infoTitle1);
+
+        ComponentRenderer _toolBarComponentRenderer = new ComponentRenderer<>(entityStatisticsInfo -> {
+            Icon queryIcon = new Icon(VaadinIcon.INPUT);
+            queryIcon.setSize("20px");
+            Button addAsAttributeKind = new Button(queryIcon, event -> {
+                if(entityStatisticsInfo instanceof EntityStatisticsInfo){
+                    //renderConceptionKindQueryUI((EntityStatisticsInfo)entityStatisticsInfo);
+                }
+            });
+            addAsAttributeKind.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+            addAsAttributeKind.addThemeVariants(ButtonVariant.LUMO_SMALL);
+            Tooltips.getCurrent().setTooltip(addAsAttributeKind, "添加为属性类型");
+
+            HorizontalLayout buttons = new HorizontalLayout(addAsAttributeKind);
+            buttons.setPadding(false);
+            buttons.setSpacing(false);
+            buttons.setMargin(false);
+            buttons.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+            buttons.setHeight(10,Unit.PIXELS);
+            buttons.setWidth(80,Unit.PIXELS);
+            return new VerticalLayout(buttons);
+        });
+
+        conceptionKindAttributesInfoGrid = new Grid<>();
+        conceptionKindAttributesInfoGrid.setWidth(100,Unit.PERCENTAGE);
+        conceptionKindAttributesInfoGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        conceptionKindAttributesInfoGrid.addThemeVariants(GridVariant.LUMO_COMPACT,GridVariant.LUMO_NO_BORDER,GridVariant.LUMO_ROW_STRIPES);
+        conceptionKindAttributesInfoGrid.addColumn(KindEntityAttributeRuntimeStatistics::getAttributeName).setHeader("属性名称").setKey("idx_0");
+        conceptionKindAttributesInfoGrid.addColumn(KindEntityAttributeRuntimeStatistics::getAttributeDataType).setHeader("属性数据类型").setKey("idx_1").setFlexGrow(0).setWidth("130px").setResizable(false);
+        conceptionKindAttributesInfoGrid.addColumn(new NumberRenderer<>(KindEntityAttributeRuntimeStatistics::getSampleCount, NumberFormat.getIntegerInstance()))
+                .setComparator((entityStatisticsInfo1, entityStatisticsInfo2) ->
+                        (int)(entityStatisticsInfo1.getAttributeHitCount() - entityStatisticsInfo2.getAttributeHitCount()))
+                .setHeader("属性采样数").setKey("idx_2")
+                .setFlexGrow(0).setWidth("100px").setResizable(false);
+        conceptionKindAttributesInfoGrid.addColumn(new NumberRenderer<>(KindEntityAttributeRuntimeStatistics::getAttributeHitCount, NumberFormat.getIntegerInstance()))
+                .setComparator((entityStatisticsInfo1, entityStatisticsInfo2) ->
+                        (int)(entityStatisticsInfo1.getAttributeHitCount() - entityStatisticsInfo2.getAttributeHitCount()))
+                .setHeader("属性命中数").setKey("idx_3")
+                .setFlexGrow(0).setWidth("100px").setResizable(false);
+        conceptionKindAttributesInfoGrid.addColumn(_toolBarComponentRenderer).setHeader("操作").setKey("idx_4").setFlexGrow(0).setWidth("60px").setResizable(false);
+        /*
+        LightGridColumnHeader gridColumnHeader_1_idx0 = new LightGridColumnHeader(VaadinIcon.BULLETS,"属性名称");
+        conceptionKindAttributesInfoGrid.getColumnByKey("idx_0").setHeader(gridColumnHeader_1_idx0).setSortable(true)
+                .setTooltipGenerator(kindEntityAttributeRuntimeStatistics -> getAttributeName(kindEntityAttributeRuntimeStatistics));
+        */
+
+        LightGridColumnHeader gridColumnHeader_1_idx1 = new LightGridColumnHeader(VaadinIcon.PASSWORD,"属性数据类型");
+        conceptionKindAttributesInfoGrid.getColumnByKey("idx_1").setHeader(gridColumnHeader_1_idx1).setSortable(true);
+        LightGridColumnHeader gridColumnHeader_1_idx2 = new LightGridColumnHeader(VaadinIcon.EYEDROPPER,"属性采样数");
+        conceptionKindAttributesInfoGrid.getColumnByKey("idx_2").setHeader(gridColumnHeader_1_idx2).setSortable(true);
+        LightGridColumnHeader gridColumnHeader_1_idx3 = new LightGridColumnHeader(VaadinIcon.CROSSHAIRS,"属性命中数");
+        conceptionKindAttributesInfoGrid.getColumnByKey("idx_3").setHeader(gridColumnHeader_1_idx3).setSortable(true);
+        LightGridColumnHeader gridColumnHeader_idx4 = new LightGridColumnHeader(VaadinIcon.TOOLS,"操作");
+        conceptionKindAttributesInfoGrid.getColumnByKey("idx_4").setHeader(gridColumnHeader_idx4);
+
+        conceptionKindAttributesInfoGrid.setHeight(218,Unit.PIXELS);
+        leftSideContainerLayout.add(conceptionKindAttributesInfoGrid);
 
 
     }
