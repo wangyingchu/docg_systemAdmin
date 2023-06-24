60850d133624329de0efba0a2c8e56cf62c6afaa
wangyingchu <yingchuwang@gmail.com>
2023-06-24 14:40:53 +0800

    update vaadin to version 24.1.1

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-progress-bar-3b53bb70-3c6a33ae.js
@@ -0,0 +1,1 @@
+import{U as r}from"./indexhtml-e1ba3ca2.js";const e={tagName:"vaadin-progress-bar",displayName:"ProgressBar",elements:[{selector:"vaadin-progress-bar::part(bar)",displayName:"Bar",properties:[r.backgroundColor]},{selector:"vaadin-progress-bar::part(value)",displayName:"Value",properties:[r.backgroundColor]}]};export{e as default};
