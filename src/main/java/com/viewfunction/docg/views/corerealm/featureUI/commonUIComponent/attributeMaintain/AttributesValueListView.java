da2be2991493d46104a419ab0c0c7cbb16c3aaa6
wangyingchu <yingchuwang@gmail.com>
2023-07-16 19:32:37 +0800

    update for AttributesValueListView feature

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/attributeMaintain/AttributesValueListView.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.attributeMaintain;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class AttributesValueListView extends VerticalLayout {
+}
