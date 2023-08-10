9d573a693c239420c5597a4115fc02910d122961
wangyingchu <yingchuwang@gmail.com>
2023-08-10 23:22:54 +0800

    update for font modification

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindRuntimeConfigurationInfoView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindRuntimeConfigurationInfoView.java
@@ -5,7 +5,9 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.viewfunction.docg.element.commonComponent.SectionActionBar;
+import com.viewfunction.docg.element.commonComponent.SecondaryIconTitle;
+import com.viewfunction.docg.element.commonComponent.SecondaryTitleActionBar;
+import com.viewfunction.docg.element.commonComponent.ThirdLevelIconTitle;
 
 public class ContainerAttributesViewKindRuntimeConfigurationInfoView extends VerticalLayout {
 
@@ -40,14 +42,27 @@ public class ContainerAttributesViewKindRuntimeConfigurationInfoView extends Ver
 
         rightSideContainerLayout = new VerticalLayout();
         rightSideContainerLayout.setWidth(400, Unit.PIXELS);
-        rightSideContainerLayout.setSpacing(false);
         rightSideContainerLayout.setMargin(false);
         rightSideContainerLayout.setPadding(false);
         mainContainerLayout.add(rightSideContainerLayout);
 
-        Icon icon2 = new Icon(VaadinIcon.INPUT);
-        SectionActionBar sectionActionBar2 = new SectionActionBar(icon2,"包含属性类型配置管理",null);
-        rightSideContainerLayout.add(sectionActionBar2);
+        HorizontalLayout spaceDiv01Layout1 = new HorizontalLayout();
+        spaceDiv01Layout1.setHeight(10,Unit.PIXELS);
+        rightSideContainerLayout.add(spaceDiv01Layout1);
+
+        SecondaryIconTitle filterTitle = new SecondaryIconTitle(new Icon(VaadinIcon.LAPTOP),"属性视图类型概览");
+        rightSideContainerLayout.add(filterTitle);
+
+        HorizontalLayout spaceDiv01Layout2 = new HorizontalLayout();
+        spaceDiv01Layout2.setHeight(2,Unit.PIXELS);
+        rightSideContainerLayout.add(spaceDiv01Layout2);
+
+        SecondaryTitleActionBar secondaryTitleActionBar = new SecondaryTitleActionBar(new Icon(VaadinIcon.TASKS),"-",null,null);
+        secondaryTitleActionBar.setWidth(100,Unit.PERCENTAGE);
+        rightSideContainerLayout.add(secondaryTitleActionBar);
+
+        ThirdLevelIconTitle infoTitle1 = new ThirdLevelIconTitle(new Icon(VaadinIcon.ALIGN_LEFT),"概念类型属性分布 (实体概略采样数 )");
+        rightSideContainerLayout.add(infoTitle1);
     }
 
     public void setViewHeight(int viewHeight){
