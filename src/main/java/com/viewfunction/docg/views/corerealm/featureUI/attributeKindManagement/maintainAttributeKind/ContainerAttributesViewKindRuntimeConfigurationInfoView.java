7dbc74ed80d887f7d81352d7197afc3597ddeae7
wangyingchu <yingchuwang@gmail.com>
2023-08-10 17:38:28 +0800

    Update for AttributeKindDetailUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindRuntimeConfigurationInfoView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindRuntimeConfigurationInfoView.java
@@ -5,42 +5,37 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.viewfunction.docg.element.commonComponent.SectionActionBar;
-import com.viewfunction.docg.views.corerealm.featureUI.attributesViewKindManagement.maintainAttributesViewKind.ContainerConceptionKindsConfigView;
-import com.viewfunction.docg.views.corerealm.featureUI.attributesViewKindManagement.maintainAttributesViewKind.ContainsAttributeKindsConfigView;
 
-public class ContainerAttributesViewKindRuntimeConfigurationInfoView extends VerticalLayout {
+import com.viewfunction.docg.element.commonComponent.SectionActionBar;
+
+public class ContainerAttributesViewKindRuntimeConfigurationInfoView extends HorizontalLayout {
 
     private VerticalLayout leftSideContainerLayout;
     private VerticalLayout rightSideContainerLayout;
+    private ContainerAttributesViewKindsConfigView containerAttributesViewKindsConfigView;
 
     private String attributeKindUID;
     public ContainerAttributesViewKindRuntimeConfigurationInfoView(String attributeKindUID){
+        setSpacing(false);
+        setMargin(false);
+        setPadding(false);
+
         this.attributeKindUID = attributeKindUID;
-        HorizontalLayout mainContainerLayout = new HorizontalLayout();
-        mainContainerLayout.setWidthFull();
-        add(mainContainerLayout);
 
         leftSideContainerLayout = new VerticalLayout();
-        mainContainerLayout.add(leftSideContainerLayout);
-        Icon icon = new Icon(VaadinIcon.CUBE);
-        icon.setSize("8px");
-        SectionActionBar sectionActionBar1 = new SectionActionBar(icon,"相关概念类型配置管理",null);
-        leftSideContainerLayout.add(sectionActionBar1);
-        //containerConceptionKindsConfigView = new ContainerConceptionKindsConfigView(this.attributesViewKindUID);
-        //leftSideContainerLayout.add(containerConceptionKindsConfigView);
+        leftSideContainerLayout.setSpacing(false);
+        leftSideContainerLayout.setMargin(false);
+        leftSideContainerLayout.setPadding(false);
+        add(leftSideContainerLayout);
 
+        containerAttributesViewKindsConfigView = new ContainerAttributesViewKindsConfigView(this.attributeKindUID);
+        leftSideContainerLayout.add(containerAttributesViewKindsConfigView);
 
         rightSideContainerLayout = new VerticalLayout();
         rightSideContainerLayout.setWidth(100, Unit.PERCENTAGE);
-        mainContainerLayout.add(rightSideContainerLayout);
+        add(rightSideContainerLayout);
         Icon icon2 = new Icon(VaadinIcon.INPUT);
         SectionActionBar sectionActionBar2 = new SectionActionBar(icon2,"包含属性类型配置管理",null);
         rightSideContainerLayout.add(sectionActionBar2);
-        //containsAttributeKindsConfigView = new ContainsAttributeKindsConfigView(this.attributesViewKindUID);
-        //rightSideContainerLayout.add(containsAttributeKindsConfigView);
-
-        mainContainerLayout.setFlexGrow(0.5,leftSideContainerLayout);
-        mainContainerLayout.setFlexGrow(0.5,rightSideContainerLayout);
     }
 }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindsConfigView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/attributeKindManagement/maintainAttributeKind/ContainerAttributesViewKindsConfigView.java
@@ -1,6 +1,136 @@
 package com.viewfunction.docg.views.corerealm.featureUI.attributeKindManagement.maintainAttributeKind;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeKind;
+import com.viewfunction.docg.element.commonComponent.GridColumnHeader;
+import com.viewfunction.docg.element.commonComponent.SecondaryTitleActionBar;
+import com.viewfunction.docg.element.commonComponent.lineAwesomeIcon.LineAwesomeIconsSvg;
+import dev.mett.vaadin.tooltip.Tooltips;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContainerAttributesViewKindsConfigView extends VerticalLayout {
+    private String attributeKindUID;
+    private Grid<AttributeKind> attributeKindGrid;
+    public ContainerAttributesViewKindsConfigView(String attributeKindUID){
+        this.attributeKindUID = attributeKindUID;
+
+        this.setWidth(100, Unit.PERCENTAGE);
+        List<Component> secTitleElementsList = new ArrayList<>();
+        List<Component> buttonList = new ArrayList<>();
+
+        Button createMetaConfigItemButton= new Button("附加新的属性视图类型");
+        createMetaConfigItemButton.setIcon(VaadinIcon.PLUS_SQUARE_O.create());
+        createMetaConfigItemButton.addThemeVariants(ButtonVariant.LUMO_ICON,ButtonVariant.LUMO_SMALL,ButtonVariant.LUMO_TERTIARY);
+        createMetaConfigItemButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                //renderAttachNewAttributeKindUI();
+            }
+        });
+        buttonList.add(createMetaConfigItemButton);
+
+        Button refreshMetaConfigItemsInfoButton = new Button("刷新包含的属性视图类型信息",new Icon(VaadinIcon.REFRESH));
+        refreshMetaConfigItemsInfoButton.addThemeVariants(ButtonVariant.LUMO_ICON,ButtonVariant.LUMO_SMALL,ButtonVariant.LUMO_TERTIARY);
+        refreshMetaConfigItemsInfoButton.addClickListener((ClickEvent<Button> click) ->{
+            //refreshAttributeTypesInfo();
+        });
+        buttonList.add(refreshMetaConfigItemsInfoButton);
+
+        SecondaryTitleActionBar metaConfigItemConfigActionBar = new SecondaryTitleActionBar(new Icon(VaadinIcon.TASKS),"属性视图类型配置管理 ",secTitleElementsList,buttonList);
+        add(metaConfigItemConfigActionBar);
+
+
+
+
+
+
+        ComponentRenderer _toolBarComponentRenderer = new ComponentRenderer<>(attributeKind -> {
+            Icon configAttachmentInfoIcon = new Icon(VaadinIcon.BULLETS);
+            configAttachmentInfoIcon.setSize("18px");
+            Button configAttachmentInfoButton = new Button(configAttachmentInfoIcon, event -> {});
+            configAttachmentInfoButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+            configAttachmentInfoButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+
+            Tooltips.getCurrent().setTooltip(configAttachmentInfoButton, "属性类型附加元数据配置管理");
+            configAttachmentInfoButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+                @Override
+                public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                    if(attributeKind instanceof AttributeKind){
+                        //renderConfigAttributeKindAttachmentMetaDataUI((AttributeKind)attributeKind);
+                    }
+                }
+            });
+
+            Icon deleteKindIcon = new Icon(VaadinIcon.TRASH);
+            deleteKindIcon.setSize("21px");
+            Button removeAttributeKindButton = new Button(deleteKindIcon, event -> {});
+            removeAttributeKindButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+            removeAttributeKindButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+            removeAttributeKindButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+            Tooltips.getCurrent().setTooltip(removeAttributeKindButton, "移除属性类型");
+            removeAttributeKindButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+                @Override
+                public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                    if(attributeKind instanceof AttributeKind){
+                        //renderDetachAttributeKindUI((AttributeKind)attributeKind);
+                    }
+                }
+            });
+
+            HorizontalLayout buttons = new HorizontalLayout(configAttachmentInfoButton,removeAttributeKindButton);
+            buttons.setPadding(false);
+            buttons.setSpacing(false);
+            buttons.setMargin(false);
+            buttons.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+            buttons.setHeight(15,Unit.PIXELS);
+            buttons.setWidth(80,Unit.PIXELS);
+            return new VerticalLayout(buttons);
+        });
+
+
+
+
+
+
+        attributeKindGrid = new Grid<>();
+        attributeKindGrid.setWidth(100,Unit.PERCENTAGE);
+        attributeKindGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        attributeKindGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        attributeKindGrid.addColumn(AttributeKind::getAttributeKindName).setHeader("属性类型名称").setKey("idx_0").setFlexGrow(1);
+        attributeKindGrid.addColumn(AttributeKind::getAttributeKindDesc).setHeader("属性类型描述").setKey("idx_1").setFlexGrow(1)
+                .setTooltipGenerator(attributeKindMetaInfoData -> attributeKindMetaInfoData.getAttributeKindDesc());
+        attributeKindGrid.addColumn(AttributeKind::getAttributeDataType).setHeader("属性数据类型").setKey("idx_2")
+                .setFlexGrow(0).setWidth("130px").setResizable(false);
+        attributeKindGrid.addColumn(AttributeKind::getAttributeKindUID).setHeader("属性类型 UID").setKey("idx_3")
+                .setFlexGrow(0).setWidth("150px").setResizable(false);
+        attributeKindGrid.addColumn(_toolBarComponentRenderer).setHeader("操作").setKey("idx_4").setFlexGrow(0).setWidth("90px").setResizable(false);
+
+        GridColumnHeader gridColumnHeader_idx0 = new GridColumnHeader(VaadinIcon.INFO_CIRCLE_O,"属性类型名称");
+        attributeKindGrid.getColumnByKey("idx_0").setHeader(gridColumnHeader_idx0).setSortable(true);
+        GridColumnHeader gridColumnHeader_idx1 = new GridColumnHeader(VaadinIcon.DESKTOP,"属性类型描述");
+        attributeKindGrid.getColumnByKey("idx_1").setHeader(gridColumnHeader_idx1).setSortable(true);
+        GridColumnHeader gridColumnHeader_idx2 = new GridColumnHeader(LineAwesomeIconsSvg.FIRSTDRAFT.create(),"属性数据类型");
+        attributeKindGrid.getColumnByKey("idx_2").setHeader(gridColumnHeader_idx2).setSortable(true);
+        GridColumnHeader gridColumnHeader_idx3 = new GridColumnHeader(VaadinIcon.KEY_O,"属性类型 UID");
+        attributeKindGrid.getColumnByKey("idx_3").setHeader(gridColumnHeader_idx3).setSortable(true);
+        GridColumnHeader gridColumnHeader_idx4 = new GridColumnHeader(VaadinIcon.TOOLS,"操作");
+        attributeKindGrid.getColumnByKey("idx_4").setHeader(gridColumnHeader_idx4);
+
+        attributeKindGrid.appendFooterRow();
+        add(attributeKindGrid);
+    }
 }
