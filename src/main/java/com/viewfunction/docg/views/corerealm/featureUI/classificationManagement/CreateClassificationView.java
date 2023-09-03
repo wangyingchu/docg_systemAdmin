9179dce122a691a6cfb44c9b46238a31b6a5420c
wangyingchu <yingchuwang@gmail.com>
2023-09-03 22:55:17 +0800

    Update for ClassificationManagementUI feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/classificationManagement/CreateClassificationView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/classificationManagement/CreateClassificationView.java
@@ -1,4 +1,10 @@
 package com.viewfunction.docg.views.corerealm.featureUI.classificationManagement;
 
-public class CreateClassificationView {
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.H6;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class CreateClassificationView extends VerticalLayout {
+    private Dialog containerDialog;
+    private H6 errorMessage;
 }
