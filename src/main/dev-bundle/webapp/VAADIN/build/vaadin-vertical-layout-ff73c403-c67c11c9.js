67f8f126e2d369e4d97bf076d60beddd1ca91626
wangyingchu <yingchuwang@gmail.com>
2023-09-21 11:44:50 +0800

    Update vaadin to 24.1.10

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-vertical-layout-ff73c403-c67c11c9.js
@@ -0,0 +1,1 @@
+import{U as a}from"./indexhtml-a17b5b05.js";const o={tagName:"vaadin-vertical-layout",displayName:"VerticalLayout",elements:[{selector:"vaadin-vertical-layout",displayName:"Layout",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius,a.padding,a.gap]}]};export{o as default};
