8a7737edee54a9287336fcbb1176bf0537b09ecb
wangyingchu <yingchuwang@gmail.com>
2023-10-23 10:59:16 +0800

    Update vaadin to 24.2.0

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-map-d40a0116-001ba0c0.js
@@ -0,0 +1,1 @@
+import{R as a}from"./indexhtml-86fd636d.js";const o={tagName:"vaadin-map",displayName:"Map",elements:[{selector:"vaadin-map",displayName:"Root element",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius,a.padding]}]};export{o as default};
---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-virtual-list-96896203-c639972d.js
@@ -0,0 +1,1 @@
+import{R as r}from"./indexhtml-86fd636d.js";const t={tagName:"vaadin-virtual-list",displayName:"Virtual List",elements:[{selector:"vaadin-virtual-list",displayName:"List",properties:[r.backgroundColor,r.borderColor,r.borderWidth,r.borderRadius]}]};export{t as default};
