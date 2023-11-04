58915592a194946200e0439c9e216d348b0a595e
wangyingchu <yingchuwang@gmail.com>
2023-11-04 17:06:01 +0800

    Update for TimeFlowDetailUI

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/timeFlowManagement/maintainTimeFlow/TimeFlowCorrelationInfoChart.java
@@ -0,0 +1,8 @@
+package com.viewfunction.docg.views.corerealm.featureUI.timeFlowManagement.maintainTimeFlow;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class TimeFlowCorrelationInfoChart extends VerticalLayout {
+
+    public TimeFlowCorrelationInfoChart(){}
+}
