7fd9961c4e2a28c9b02986282fe855cd5fe2f0c8
wangyingchu <yingchuwang@gmail.com>
2023-12-06 16:53:28 +0800

    update for ComputeGridView feature

---
+++ src/main/java/com/viewfunction/docg/element/commonComponent/chart/BulletChart.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.element.commonComponent.chart;
+
+import com.vaadin.flow.component.html.Div;
+
+public class BulletChart extends Div {
+}
--- src/main/java/com/viewfunction/docg/views/computegrid/featureUI/dataComputeGridManagement/GridRuntimeInfoWidget.java
+++ src/main/java/com/viewfunction/docg/views/computegrid/featureUI/dataComputeGridManagement/GridRuntimeInfoWidget.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeGrid.ComputeGridRealtimeMetrics;
 import com.viewfunction.docg.element.commonComponent.SecondaryKeyValueDisplayItem;
 import com.viewfunction.docg.element.commonComponent.ThirdLevelIconTitle;
+import com.viewfunction.docg.element.commonComponent.chart.BulletChart;
 import com.viewfunction.docg.element.commonComponent.chart.PieChart;
 import com.viewfunction.docg.element.commonComponent.lineAwesomeIcon.LineAwesomeIconsSvg;
 
@@ -78,6 +79,57 @@ public class GridRuntimeInfoWidget extends VerticalLayout {
 
         ThirdLevelIconTitle memoryStatusInfoTitle = new ThirdLevelIconTitle(LineAwesomeIconsSvg.MEMORY_SOLID.create(), "网格内存资源概览");
         add(memoryStatusInfoTitle);
+
+
+        HorizontalLayout heapMemorySpaceInfoLayout = new HorizontalLayout();
+        heapMemorySpaceInfoLayout.setDefaultVerticalComponentAlignment(Alignment.START);
+        add(heapMemorySpaceInfoLayout);
+
+        VerticalLayout heapMemoryInfoLeftLayout = new VerticalLayout();
+        heapMemoryInfoLeftLayout.setPadding(false);
+        heapMemoryInfoLeftLayout.setDefaultHorizontalComponentAlignment(Alignment.START);
+        heapMemorySpaceInfoLayout.add(heapMemoryInfoLeftLayout);
+
+        HorizontalLayout statusInfoContainer7 = new HorizontalLayout();
+        statusInfoContainer7.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        heapMemoryInfoLeftLayout.add(statusInfoContainer7);
+        totalDiskDisplayItem = new SecondaryKeyValueDisplayItem(statusInfoContainer7, VaadinIcon.DATABASE.create(),"可用HEAP内存总量:",""+"1,000,000"+" GB");
+
+        HorizontalLayout statusInfoContainer8 = new HorizontalLayout();
+        statusInfoContainer8.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        heapMemoryInfoLeftLayout.add(statusInfoContainer8);
+        freeDiskDisplayItem = new SecondaryKeyValueDisplayItem(statusInfoContainer8, VaadinIcon.HARDDRIVE.create(),"已用HEAP内存总量:","1,000,000"+" GB");
+
+        HorizontalLayout statusInfoContainer9 = new HorizontalLayout();
+        statusInfoContainer9.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        heapMemoryInfoLeftLayout.add(statusInfoContainer9);
+        SecondaryKeyValueDisplayItem freeDiskDisplayItem2 = new SecondaryKeyValueDisplayItem(statusInfoContainer9, VaadinIcon.HARDDRIVE.create(),"HEAP内存最大使用量:","1,000,000"+" GB");
+
+        HorizontalLayout statusInfoContainer10 = new HorizontalLayout();
+        statusInfoContainer10.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        heapMemoryInfoLeftLayout.add(statusInfoContainer10);
+        freeDiskPercentDisplayItem = new SecondaryKeyValueDisplayItem(statusInfoContainer10, VaadinIcon.PIE_CHART.create(),"系统未用磁盘空间占比:","59%");
+
+        HorizontalLayout statusInfoContainer11 = new HorizontalLayout();
+        statusInfoContainer11.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        heapMemoryInfoLeftLayout.add(statusInfoContainer11);
+        usableDiskDisplayItem = new SecondaryKeyValueDisplayItem(statusInfoContainer11, VaadinIcon.HARDDRIVE_O.create(),"领域可用磁盘空间总量:","77%");
+
+        BulletChart bulletChart1 = new BulletChart();
+        heapMemorySpaceInfoLayout.add(bulletChart1);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     public void refreshRuntimeInfo(ComputeGridRealtimeMetrics targetComputeGridRealtimeMetrics){
