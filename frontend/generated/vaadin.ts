f9d4ab945425e5ffabcebef500f6f4b953f0e25a
wangyingchu <yingchuwang@gmail.com>
2023-12-14 22:33:00 +0800

    Update vaadin to 24.3.0

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -1,3 +1,11 @@
+import 'Frontend/generated/jar-resources/copilot/copilot.js';
+import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/context-menu/src/vaadin-context-menu.js';
+import '@vaadin/icons/vaadin-iconset.js';
+import '@vaadin/icon/vaadin-icon.js';
+import '@vaadin/text-field/src/vaadin-text-field.js';
+import '@vaadin/grid/src/vaadin-grid.js';
+import '@vaadin/grid/src/vaadin-grid-tree-column.js';
 import './vaadin-featureflags.js';
 
 import './index';
