016680f20def9c902fd8ffcf85ae48c91a77aa5f
wangyingchu <yingchuwang@gmail.com>
2023-12-15 17:00:16 +0800

    update for ComputeGridView feature

---
+++ frontend/visualization/common/bulletChart_amcharts-connector.js
@@ -0,0 +1,200 @@
+window.Vaadin.Flow.common_BulletChart_amcharts = {
+    initLazy: function (c) {
+        // Check whether the connector was already initialized
+        if (c.$connector) {
+            return;
+        }
+        c.$connector = {
+            // functions
+        };
+
+
+
+
+
+
+
+
+        let colors = [
+            am5.color(0x19d228),
+            am5.color(0xb4dd1e),
+            am5.color(0xf4fb16),
+            am5.color(0xf6d32b),
+            am5.color(0xfb7116)
+        ];
+
+
+
+
+
+        // Create root element
+        // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+        let root = am5.Root.new(c);
+
+        // Set themes
+        // https://www.amcharts.com/docs/v5/concepts/themes/
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+
+        // Create chart
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/
+        let chart = root.container.children.push(
+            am5xy.XYChart.new(root, {
+                panX: false,
+                panY: false,
+                wheelX: "none",
+                wheelY: "none",
+                arrangeTooltips: false
+            })
+        );
+
+        // Create axes
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+        let yAxis = chart.yAxes.push(
+            am5xy.CategoryAxis.new(root, {
+                categoryField: "category",
+                renderer: am5xy.AxisRendererY.new(root, {})
+            })
+        );
+
+        yAxis.data.setAll([{ category: "Evaluation" }]);
+
+        let xRenderer = am5xy.AxisRendererX.new(root, {});
+        xRenderer.grid.template.set("forceHidden", true);
+
+        let xAxis = chart.xAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: xRenderer,
+                min: 0,
+                max: 100,
+                numberFormat: "#.'%'"
+            })
+        );
+
+
+
+
+        let rangeDataItem = xAxis.makeDataItem({ value: 0, endValue: 100 });
+        let range = xAxis.createAxisRange(rangeDataItem);
+
+        rangeDataItem.get("axisFill").setAll({
+            visible: true,
+            fillOpacity:1
+        });
+
+        let stops = [];
+        for (var i = 0; i < colors.length; i++) {
+            stops.push({ color: colors[i] });
+        }
+
+        let linearGradient = am5.LinearGradient.new(root, {
+            rotation: 0,
+            stops: stops
+        });
+
+        rangeDataItem.get("axisFill").set("fillGradient", linearGradient);
+
+
+
+
+
+
+
+/*
+        let count = colors.length;
+        for (var i = 0; i < count; i++) {
+            let rangeDataItem = xAxis.makeDataItem({
+                value: (i / count) * 100,
+                endValue: ((i + 1) / count) * 100
+            });
+            let range = xAxis.createAxisRange(rangeDataItem);
+
+            rangeDataItem.get("axisFill").setAll({
+                visible: true,
+                fill: colors[i],
+                stroke: colors[i],
+                fillOpacity:1
+            });
+        }
+*/
+
+
+
+
+        let series = chart.series.push(
+            am5xy.ColumnSeries.new(root, {
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueXField: "value",
+                categoryYField: "category",
+                fill: am5.color(0x000000),
+                stroke: am5.color(0x000000),
+                tooltip: am5.Tooltip.new(root, {
+                    pointerOrientation: "left",
+                    labelText: "{valueX}%"
+                })
+            })
+        );
+
+        series.columns.template.setAll({
+            height: am5.p50
+        });
+
+        series.data.setAll([{ category: "Evaluation", value: 65 }]);
+
+        let stepSeries = chart.series.push(
+            am5xy.StepLineSeries.new(root, {
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueXField: "value",
+                categoryYField: "category",
+                stroke: am5.color(0x000000),
+                fill: am5.color(0x000000),
+                noRisers: true,
+                stepWidth: am5.p50,
+                tooltip: am5.Tooltip.new(root, {
+                    pointerOrientation: "left",
+                    labelText: "{valueX}%"
+                })
+            })
+        );
+
+        stepSeries.strokes.template.set("strokeWidth", 3);
+        stepSeries.data.setAll([{ category: "Evaluation", value: 83 }]);
+
+        // Add cursor
+        // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+        let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+                behavior: "none"
+            })
+        );
+        cursor.lineY.set("visible", false);
+        cursor.lineX.set("visible", false);
+
+        // Make stuff animate on load
+        // https://www.amcharts.com/docs/v5/concepts/animations/
+        chart.appear(1000, 100);
+
+        series.appear();
+        stepSeries.appear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/main/java/com/viewfunction/docg/element/commonComponent/chart/BulletChart.java
+++ src/main/java/com/viewfunction/docg/element/commonComponent/chart/BulletChart.java
@@ -1,10 +1,31 @@
 package com.viewfunction.docg.element.commonComponent.chart;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.function.SerializableConsumer;
 
+@JavaScript("./visualization/common/bulletChart_amcharts-connector.js")
 public class BulletChart extends Div {
 
     public BulletChart(){
-        this.getStyle().set("background-color","#CE0000");
+
+        UI.getCurrent().getPage().addJavaScript("js/amcharts/5.4.5/index.js");
+        UI.getCurrent().getPage().addJavaScript("js/amcharts/5.4.5/hierarchy.js");
+        UI.getCurrent().getPage().addJavaScript("js/amcharts/5.4.5/xy.js");
+        UI.getCurrent().getPage().addJavaScript("js/amcharts/5.4.5/themes/Animated.js");
+
+        initConnector(getElement());
+    }
+
+    private void initConnector(Element layout) {
+        runBeforeClientResponse(ui -> ui.getPage().executeJs(
+                "window.Vaadin.Flow.common_BulletChart_amcharts.initLazy($0)", getElement()));
+    }
+
+    private void runBeforeClientResponse(SerializableConsumer<UI> command) {
+        getElement().getNode().runWhenAttached(ui -> ui
+                .beforeClientResponse(this, context -> command.accept(ui)));
     }
 }
