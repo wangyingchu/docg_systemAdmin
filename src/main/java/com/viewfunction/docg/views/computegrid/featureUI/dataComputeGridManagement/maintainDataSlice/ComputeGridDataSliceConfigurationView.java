976602e49ed588b8ce5241b5c69284bd3e7f49f7
wangyingchu <yingchuwang@gmail.com>
2023-12-24 20:03:48 +0800

    Update for DataCompute model feature

--- src/main/java/com/viewfunction/docg/views/computegrid/featureUI/dataComputeGridManagement/maintainDataSlice/ComputeGridDataSliceConfigurationView.java
+++ src/main/java/com/viewfunction/docg/views/computegrid/featureUI/dataComputeGridManagement/maintainDataSlice/ComputeGridDataSliceConfigurationView.java
@@ -43,6 +43,14 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
     private Grid<DataSliceMetaInfo> dataSliceMetaInfoGrid;
     private DataSliceMetaInfo lastSelectedDataSliceMetaInfo;
     private Grid<DataSlicePropertyDefinitionVO> dataSlicePropertyDefinitionsGrid;
+    private SecondaryKeyValueDisplayItem groupNameDisplayItem;
+    private SecondaryKeyValueDisplayItem primaryDataCountDisplayItem;
+    private SecondaryKeyValueDisplayItem backupDataCountDisplayItem;
+    private SecondaryKeyValueDisplayItem totalDataCountDisplayItem;
+    private SecondaryKeyValueDisplayItem sliceAtomicityDisplayItem;
+    private SecondaryKeyValueDisplayItem backupNumberDisplayItem;
+    private SecondaryKeyValueDisplayItem sliceStorageModeDisplayItem;
+
     private class DataSlicePropertyDefinitionVO{
         private String propertyName;
         private DataSlicePropertyType dataSlicePropertyType;
@@ -77,6 +85,7 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         add(infoContainer);
 
         this.numberFormat = NumberFormat.getInstance();
+
         this.gridDataSlicesCountDisplayItem =
                 new PrimaryKeyValueDisplayItem(infoContainer, FontAwesome.Solid.CIRCLE.create()," 网格数据切片数量:","-");
 
@@ -233,7 +242,7 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
             }
         });
         dataSliceMetaInfoGrid.addColumn(DataSliceMetaInfo::getStoreBackupNumber).setHeader("备份数量").setKey("idx_2").setWidth("100px").setFlexGrow(0).setResizable(false);
-        dataSliceMetaInfoGrid.addColumn(new NumberRenderer<>(DataSliceMetaInfo::getPrimaryDataCount,NumberFormat.getIntegerInstance())).setHeader("数据量").setKey("idx_3").setWidth("130px").setFlexGrow(0);
+        dataSliceMetaInfoGrid.addColumn(new NumberRenderer<>(DataSliceMetaInfo::getPrimaryDataCount,NumberFormat.getIntegerInstance())).setHeader("切片数据量").setKey("idx_3").setWidth("130px").setFlexGrow(0);
         dataSliceMetaInfoGrid.addColumn(_toolBarComponentRenderer).setHeader("操作").setKey("idx_4").setFlexGrow(0).setWidth("140px").setResizable(false);
 
         LightGridColumnHeader gridColumnHeader_idx0 = new LightGridColumnHeader(VaadinIcon.INFO_CIRCLE_O,"切片名称");
@@ -242,7 +251,7 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         dataSliceMetaInfoGrid.getColumnByKey("idx_1").setHeader(gridColumnHeader_idx1).setSortable(true);
         LightGridColumnHeader gridColumnHeader_idx2 = new LightGridColumnHeader(VaadinIcon.FLIP_H,"备份数量");
         dataSliceMetaInfoGrid.getColumnByKey("idx_2").setHeader(gridColumnHeader_idx2).setSortable(true);
-        LightGridColumnHeader gridColumnHeader_idx3 = new LightGridColumnHeader(VaadinIcon.STOCK,"Primary 数据量");
+        LightGridColumnHeader gridColumnHeader_idx3 = new LightGridColumnHeader(VaadinIcon.STOCK,"切片主数据量");
         dataSliceMetaInfoGrid.getColumnByKey("idx_3").setHeader(gridColumnHeader_idx3).setSortable(true);
         LightGridColumnHeader gridColumnHeader_idx4 = new LightGridColumnHeader(VaadinIcon.TOOLS,"操作");
         dataSliceMetaInfoGrid.getColumnByKey("idx_4").setHeader(gridColumnHeader_idx4).setSortable(true);
@@ -280,27 +289,47 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         ThirdLevelIconTitle infoTitle2 = new ThirdLevelIconTitle(new Icon(VaadinIcon.DASHBOARD),"数据切片指标");
         rightSideLayout.add(infoTitle2);
 
+        HorizontalLayout displayItemContainer4 = new HorizontalLayout();
+        displayItemContainer4.getStyle().set("padding-left","5px");
+        rightSideLayout.add(displayItemContainer4);
+        groupNameDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer4, VaadinIcon.TAG.create(),"切片组名称:","-");
+
         HorizontalLayout displayItemContainer1 = new HorizontalLayout();
         displayItemContainer1.getStyle().set("padding-left","5px");
         rightSideLayout.add(displayItemContainer1);
-        SecondaryKeyValueDisplayItem childClassificationCount1 = new SecondaryKeyValueDisplayItem(displayItemContainer1, VaadinIcon.TAG.create(),"Child Classification-子分类数量:","-");
+        primaryDataCountDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer1, VaadinIcon.TAG.create(),"主数据量:","-");
 
-        HorizontalLayout displayItemContainer2 = new HorizontalLayout();
-        displayItemContainer2.getStyle().set("padding-left","5px");
-        rightSideLayout.add(displayItemContainer2);
-        SecondaryKeyValueDisplayItem childClassificationCount2 = new SecondaryKeyValueDisplayItem(displayItemContainer2, VaadinIcon.TAG.create(),"Child Classification-子分类数量:","-");
+        HorizontalLayout spaceDivLayout01 = new HorizontalLayout();
+        spaceDivLayout01.setWidth(5,Unit.PIXELS);
+        displayItemContainer1.add(spaceDivLayout01);
+        backupDataCountDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer1, VaadinIcon.TAG.create(),"备份数据量:","-");
 
         HorizontalLayout displayItemContainer3 = new HorizontalLayout();
         displayItemContainer3.getStyle().set("padding-left","5px");
         rightSideLayout.add(displayItemContainer3);
-        SecondaryKeyValueDisplayItem childClassificationCount3 = new SecondaryKeyValueDisplayItem(displayItemContainer3, VaadinIcon.TAG.create(),"Child Classification-子分类数量:","-");
+        totalDataCountDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer3, VaadinIcon.TAG.create(),"总数据量:","-");
+
+        HorizontalLayout spaceDivLayout02 = new HorizontalLayout();
+        spaceDivLayout02.setWidth(5,Unit.PIXELS);
+        displayItemContainer3.add(spaceDivLayout02);
+        backupNumberDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer3, VaadinIcon.TAG.create(),"切片备份数:","-");
+
+        HorizontalLayout displayItemContainer5 = new HorizontalLayout();
+        displayItemContainer5.getStyle().set("padding-left","5px");
+        rightSideLayout.add(displayItemContainer5);
+        sliceAtomicityDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer5, VaadinIcon.TAG.create(),"原子类型:","-");
+
+        HorizontalLayout spaceDivLayout03 = new HorizontalLayout();
+        spaceDivLayout03.setWidth(5,Unit.PIXELS);
+        displayItemContainer5.add(spaceDivLayout03);
+
+        sliceStorageModeDisplayItem = new SecondaryKeyValueDisplayItem(displayItemContainer5, VaadinIcon.TAG.create(),"存储类型:","-");
 
         ThirdLevelIconTitle infoTitle = new ThirdLevelIconTitle(new Icon(VaadinIcon.ALIGN_LEFT),"数据切片属性定义");
         rightSideLayout.add(infoTitle);
 
         dataSlicePropertyDefinitionsGrid = new Grid<>();
         dataSlicePropertyDefinitionsGrid.setWidth(100,Unit.PERCENTAGE);
-        dataSlicePropertyDefinitionsGrid.setHeight(400,Unit.PIXELS);
         dataSlicePropertyDefinitionsGrid.setSelectionMode(Grid.SelectionMode.NONE);
         dataSlicePropertyDefinitionsGrid.addThemeVariants(GridVariant.LUMO_COMPACT,GridVariant.LUMO_NO_BORDER,GridVariant.LUMO_ROW_STRIPES);
         dataSlicePropertyDefinitionsGrid.addColumn(DataSlicePropertyDefinitionVO::getPropertyName).setHeader("属性名称").setKey("idx_0");
@@ -312,6 +341,9 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         dataSlicePropertyDefinitionsGrid.getColumnByKey("idx_1").setHeader(gridColumnHeader_1_idx1).setSortable(true);
 
         rightSideLayout.add(dataSlicePropertyDefinitionsGrid);
+
+        VerticalLayout spaceHolderLayout = new VerticalLayout();
+        rightSideLayout.add(spaceHolderLayout);
     }
 
     @Override
@@ -320,11 +352,13 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         // Add browser window listener to observe size change
         getUI().ifPresent(ui -> listener = ui.getPage().addBrowserWindowResizeListener(event -> {
             dataSliceMetaInfoGrid.setHeight(event.getHeight()-385,Unit.PIXELS);
+            dataSlicePropertyDefinitionsGrid.setHeight(event.getHeight()-650,Unit.PIXELS);
         }));
         // Adjust size according to initial width of the screen
         getUI().ifPresent(ui -> ui.getPage().retrieveExtendedClientDetails(receiver -> {
             int browserHeight = receiver.getBodyClientHeight();
             dataSliceMetaInfoGrid.setHeight(browserHeight-385,Unit.PIXELS);
+            dataSlicePropertyDefinitionsGrid.setHeight(browserHeight-650,Unit.PIXELS);
         }));
         renderGridDateSlicesInfo();
     }
@@ -354,13 +388,13 @@ public class ComputeGridDataSliceConfigurationView extends VerticalLayout {
         try {
             DataSliceDetailInfo dataSliceDetailInfo = targetComputeGrid.getDataSliceDetail(dataSliceName);
             if(dataSliceDetailInfo != null){
-                dataSliceDetailInfo.getPrimaryDataCount();
-                dataSliceDetailInfo.getBackupDataCount();
-                dataSliceDetailInfo.getTotalDataCount();
-                dataSliceDetailInfo.getSliceGroupName();
-                dataSliceDetailInfo.getAtomicityMode();
-                dataSliceDetailInfo.getDataStoreMode();
-                dataSliceDetailInfo.getStoreBackupNumber();
+                groupNameDisplayItem.updateDisplayValue(dataSliceDetailInfo.getSliceGroupName());
+                primaryDataCountDisplayItem.updateDisplayValue(numberFormat.format(dataSliceDetailInfo.getPrimaryDataCount()));
+                backupDataCountDisplayItem.updateDisplayValue(numberFormat.format(dataSliceDetailInfo.getBackupDataCount()));
+                totalDataCountDisplayItem.updateDisplayValue(numberFormat.format(dataSliceDetailInfo.getTotalDataCount()));
+                sliceAtomicityDisplayItem.updateDisplayValue(dataSliceDetailInfo.getAtomicityMode().toString());
+                backupNumberDisplayItem.updateDisplayValue(""+dataSliceDetailInfo.getStoreBackupNumber());
+                sliceStorageModeDisplayItem.updateDisplayValue(dataSliceDetailInfo.getDataStoreMode().toString());
 
                 Map<String,DataSlicePropertyType> dataSlicePropertiesMap = dataSliceDetailInfo.getPropertiesDefinition();
                 List<DataSlicePropertyDefinitionVO> dataSlicePropertyDefinitionVOList = new ArrayList<>();
