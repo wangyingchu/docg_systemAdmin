d1cf8d02b16501e3f687c036767efeed1de93879
wangyingchu <yingchuwang@gmail.com>
2024-01-23 12:32:23 +0800

    update for AttachConceptionKindEntitiesToTimeFlowView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/attachToTimeFlowAndGeospatialRegion/AttachConceptionKindEntitiesToTimeFlowView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/attachToTimeFlowAndGeospatialRegion/AttachConceptionKindEntitiesToTimeFlowView.java
@@ -5,8 +5,10 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
@@ -15,13 +17,17 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.EntitiesOperationStatistics;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.KindEntityAttributeRuntimeStatistics;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeDataType;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeFlow;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
+import com.viewfunction.docg.element.commonComponent.ConfirmWindow;
 import com.viewfunction.docg.element.commonComponent.FixSizeWindow;
 import com.viewfunction.docg.element.commonComponent.FootprintMessageBar;
 import com.viewfunction.docg.element.commonComponent.ThirdLevelIconTitle;
@@ -30,10 +36,8 @@ import com.viewfunction.docg.element.userInterfaceUtil.CommonUIOperationUtil;
 import com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.entityMaintain.AddEntityAttributeView;
 import com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.entityMaintain.AttributeEditorItemWidget;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
     private String conceptionKindName;
@@ -42,11 +46,8 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
     private ComboBox<String> dateTimeFormatterSelect;
     private TextField eventCommentField;
     private ComboBox<TimeFlow.TimeScaleGrade> timeScaleGradeSelect;
-
-
-
-    private VerticalLayout relationEntityAttributesContainer;
-    private Map<String,AttributeEditorItemWidget> relationAttributeEditorsMap;
+    private VerticalLayout eventEntityAttributesContainer;
+    private Map<String,AttributeEditorItemWidget> eventAttributeEditorsMap;
     private Button clearAttributeButton;
 
     public AttachConceptionKindEntitiesToTimeFlowView(String conceptionKindName){
@@ -63,6 +64,8 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
         add(entityInfoFootprintMessageBar);
 
         timeEventAttributeSelect = new ComboBox<>("时间事件属性");
+        timeEventAttributeSelect.setRequired(true);
+        timeEventAttributeSelect.setRequiredIndicatorVisible(true);
         timeEventAttributeSelect.setAllowCustomValue(true);
         timeEventAttributeSelect.setPageSize(30);
         timeEventAttributeSelect.setPlaceholder("选择时间事件属性");
@@ -76,6 +79,20 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
             }
         });
         timeEventAttributeSelect.setRenderer(createRenderer());
+        timeEventAttributeSelect.addValueChangeListener(new HasValue.ValueChangeListener<AbstractField.ComponentValueChangeEvent<ComboBox<KindEntityAttributeRuntimeStatistics>, KindEntityAttributeRuntimeStatistics>>() {
+            @Override
+            public void valueChanged(AbstractField.ComponentValueChangeEvent<ComboBox<KindEntityAttributeRuntimeStatistics>,
+                    KindEntityAttributeRuntimeStatistics> comboBoxKindEntityAttributeRuntimeStatisticsComponentValueChangeEvent) {
+                KindEntityAttributeRuntimeStatistics newValue = comboBoxKindEntityAttributeRuntimeStatisticsComponentValueChangeEvent.getValue();
+                AttributeDataType attributeDataType = newValue.getAttributeDataType();
+                if(attributeDataType.equals(AttributeDataType.STRING)){
+                    dateTimeFormatterSelect.setEnabled(true);
+                }else{
+                    dateTimeFormatterSelect.setEnabled(false);
+                    dateTimeFormatterSelect.setValue(null);
+                }
+            }
+        });
         add(timeEventAttributeSelect);
 
         dateTimeFormatterSelect = new ComboBox<>("时间日期定义格式");
@@ -96,9 +113,12 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
         dateTimeFormatterSelect.setPageSize(30);
         dateTimeFormatterSelect.setPlaceholder("选择或输入时间日期定义格式");
         dateTimeFormatterSelect.setWidthFull();
+        dateTimeFormatterSelect.setEnabled(false);
         add(dateTimeFormatterSelect);
 
         timeScaleGradeSelect = new ComboBox<>("事件时间刻度");
+        timeScaleGradeSelect.setRequired(true);
+        timeScaleGradeSelect.setRequiredIndicatorVisible(true);
         timeScaleGradeSelect.setItems(TimeFlow.TimeScaleGrade.YEAR,TimeFlow.TimeScaleGrade.MONTH,
                 TimeFlow.TimeScaleGrade.DAY,TimeFlow.TimeScaleGrade.HOUR,TimeFlow.TimeScaleGrade.MINUTE);
         timeScaleGradeSelect.setAllowCustomValue(false);
@@ -108,6 +128,8 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
         add(timeScaleGradeSelect);
 
         eventCommentField = new TextField("时间事件备注");
+        eventCommentField.setRequired(true);
+        eventCommentField.setRequiredIndicatorVisible(true);
         eventCommentField.setWidthFull();
         add(eventCommentField);
 
@@ -149,13 +171,13 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
         });
         addEventAttributesUIContainerLayout.add(clearAttributeButton);
 
-        relationEntityAttributesContainer = new VerticalLayout();
-        relationEntityAttributesContainer.setMargin(false);
-        relationEntityAttributesContainer.setSpacing(false);
-        relationEntityAttributesContainer.setPadding(false);
-        relationEntityAttributesContainer.setWidth(100, Unit.PERCENTAGE);
+        eventEntityAttributesContainer = new VerticalLayout();
+        eventEntityAttributesContainer.setMargin(false);
+        eventEntityAttributesContainer.setSpacing(false);
+        eventEntityAttributesContainer.setPadding(false);
+        eventEntityAttributesContainer.setWidth(100, Unit.PERCENTAGE);
 
-        Scroller relationEntityAttributesScroller = new Scroller(relationEntityAttributesContainer);
+        Scroller relationEntityAttributesScroller = new Scroller(eventEntityAttributesContainer);
         relationEntityAttributesScroller.setHeight(150,Unit.PIXELS);
         relationEntityAttributesScroller.setScrollDirection(Scroller.ScrollDirection.VERTICAL);
         add(relationEntityAttributesScroller);
@@ -167,21 +189,17 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
                 .set("padding-bottom", "var(--lumo-space-m)");
         add(spaceDivLayout);
 
-        Button confirmButton = new Button("确认链接概念类型实体至时间流",new Icon(VaadinIcon.CHECK_CIRCLE));
+        Button confirmButton = new Button("链接概念类型实体至时间流",new Icon(VaadinIcon.CHECK_CIRCLE));
         confirmButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         add(confirmButton);
         setHorizontalComponentAlignment(Alignment.END,confirmButton);
         confirmButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
             @Override
             public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
-                //doCreateNewConceptionKind();
+                attachConceptionKindEntitiesToTimeFlow();
             }
         });
-
-
-
-
-        this.relationAttributeEditorsMap = new HashMap<>();
+        this.eventAttributeEditorsMap = new HashMap<>();
     }
 
     @Override
@@ -228,16 +246,16 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
     }
 
     private void renderAddNewAttributeUI(){
-        AddEntityAttributeView addEntityAttributeView = new AddEntityAttributeView(null,null, AddEntityAttributeView.KindType.RelationKind);
+        AddEntityAttributeView addEntityAttributeView = new AddEntityAttributeView(null,null, AddEntityAttributeView.KindType.ConceptionKind);
         AttributeValueOperateHandler attributeValueOperateHandlerForDelete = new AttributeValueOperateHandler(){
             @Override
             public void handleAttributeValue(AttributeValue attributeValue) {
                 String attributeName = attributeValue.getAttributeName();
-                if(relationAttributeEditorsMap.containsKey(attributeName)){
-                    relationEntityAttributesContainer.remove(relationAttributeEditorsMap.get(attributeName));
+                if(eventAttributeEditorsMap.containsKey(attributeName)){
+                    eventEntityAttributesContainer.remove(eventAttributeEditorsMap.get(attributeName));
                 }
-                relationAttributeEditorsMap.remove(attributeName);
-                if(relationAttributeEditorsMap.size()==0) {
+                eventAttributeEditorsMap.remove(attributeName);
+                if(eventAttributeEditorsMap.size()==0) {
                     clearAttributeButton.setEnabled(false);
                 }
             }
@@ -246,16 +264,16 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
             @Override
             public void handleAttributeValue(AttributeValue attributeValue) {
                 String attributeName = attributeValue.getAttributeName();
-                if(relationAttributeEditorsMap.containsKey(attributeName)){
-                    CommonUIOperationUtil.showPopupNotification("已经设置了名称为 "+attributeName+" 的关系属性", NotificationVariant.LUMO_ERROR);
+                if(eventAttributeEditorsMap.containsKey(attributeName)){
+                    CommonUIOperationUtil.showPopupNotification("已经设置了名称为 "+attributeName+" 的事件属性", NotificationVariant.LUMO_ERROR);
                 }else{
                     AttributeEditorItemWidget attributeEditorItemWidget = new AttributeEditorItemWidget(null,null,attributeValue, AttributeEditorItemWidget.KindType.RelationKind);
                     attributeEditorItemWidget.setWidth(445,Unit.PIXELS);
-                    relationEntityAttributesContainer.add(attributeEditorItemWidget);
+                    eventEntityAttributesContainer.add(attributeEditorItemWidget);
                     attributeEditorItemWidget.setAttributeValueOperateHandler(attributeValueOperateHandlerForDelete);
-                    relationAttributeEditorsMap.put(attributeName,attributeEditorItemWidget);
+                    eventAttributeEditorsMap.put(attributeName,attributeEditorItemWidget);
                 }
-                if(relationAttributeEditorsMap.size()>0){
+                if(eventAttributeEditorsMap.size()>0){
                     clearAttributeButton.setEnabled(true);
                 }
             }
@@ -270,8 +288,113 @@ public class AttachConceptionKindEntitiesToTimeFlowView extends VerticalLayout {
     }
 
     private void cleanRelationAttributes(){
-        relationEntityAttributesContainer.removeAll();
-        relationAttributeEditorsMap.clear();
+        eventEntityAttributesContainer.removeAll();
+        eventAttributeEditorsMap.clear();
         clearAttributeButton.setEnabled(false);
+    }
+
+    private void attachConceptionKindEntitiesToTimeFlow(){
+        KindEntityAttributeRuntimeStatistics selectedAttribute = timeEventAttributeSelect.getValue();
+        String inputDateTimeFormatter = dateTimeFormatterSelect.getValue();
+        TimeFlow.TimeScaleGrade selectedTimeScaleGrade = timeScaleGradeSelect.getValue();
+        String eventComment = eventCommentField.getValue();
+        if(selectedAttribute == null){
+            CommonUIOperationUtil.showPopupNotification("请选择时间事件属性", NotificationVariant.LUMO_ERROR,10000, Notification.Position.MIDDLE);
+            return;
+        }
+        if(selectedAttribute.getAttributeDataType().equals(AttributeDataType.STRING) && inputDateTimeFormatter == null){
+            CommonUIOperationUtil.showPopupNotification("请确定时间日期定义格式", NotificationVariant.LUMO_ERROR,10000, Notification.Position.MIDDLE);
+            return;
+        }
+        if(selectedTimeScaleGrade == null){
+            CommonUIOperationUtil.showPopupNotification("请选择事件时间刻度", NotificationVariant.LUMO_ERROR,10000, Notification.Position.MIDDLE);
+            return;
+        }
+        if(eventComment.equals("")){
+            CommonUIOperationUtil.showPopupNotification("请输入时间事件备注", NotificationVariant.LUMO_ERROR,10000, Notification.Position.MIDDLE);
+            return;
+        }
+
+        List<Button> actionButtonList = new ArrayList<>();
+
+        Button confirmButton = new Button("确认链接概念类型实体至时间流",new Icon(VaadinIcon.CHECK_CIRCLE));
+        Button cancelButton = new Button("取消操作");
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE,ButtonVariant.LUMO_SMALL);
+        actionButtonList.add(confirmButton);
+        actionButtonList.add(cancelButton);
+
+        ConfirmWindow confirmWindow = new ConfirmWindow(new Icon(VaadinIcon.INFO),"确认操作","请确认执行链接概念类型实体至时间流操作",actionButtonList,400,180);
+        confirmWindow.open();
+
+        confirmButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                doAttachConceptionKindEntitiesToTimeFlow();
+                confirmWindow.closeConfirmWindow();
+            }
+        });
+        cancelButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+            @Override
+            public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+                confirmWindow.closeConfirmWindow();
+            }
+        });
+    }
+
+    private void doAttachConceptionKindEntitiesToTimeFlow(){
+        Map<String, Object> eventData = eventAttributeEditorsMap.isEmpty() ? null : new HashMap<>();
+        if(!eventAttributeEditorsMap.isEmpty()){
+            Set<String> commentPropertiesNameSet = eventAttributeEditorsMap.keySet();
+            for(String currentPropertyName:commentPropertiesNameSet){
+                AttributeEditorItemWidget attributeEditorItemWidget = eventAttributeEditorsMap.get(currentPropertyName);
+                eventData.put(attributeEditorItemWidget.getAttributeName(),attributeEditorItemWidget.getAttributeValue());
+            }
+        }
+
+        KindEntityAttributeRuntimeStatistics selectedAttribute = timeEventAttributeSelect.getValue();
+        String inputDateTimeFormatter = dateTimeFormatterSelect.getValue();
+        String attributeName = selectedAttribute.getAttributeName();
+        AttributeDataType attributeType = selectedAttribute.getAttributeDataType();
+        DateTimeFormatter dateTimeFormatter =
+        attributeType.equals(AttributeDataType.STRING) ? DateTimeFormatter.ofPattern(inputDateTimeFormatter) : null;
+        TimeFlow.TimeScaleGrade selectedTimeScaleGrade = timeScaleGradeSelect.getValue();
+        String eventComment = eventCommentField.getValue();
+
+        try {
+            CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+            ConceptionKind targetConceptionKind = coreRealm.getConceptionKind(this.conceptionKindName);
+            EntitiesOperationStatistics attachResult = targetConceptionKind.attachTimeScaleEvents(null,attributeName,dateTimeFormatter,null,eventComment,eventData,selectedTimeScaleGrade);
+            showPopupNotification(attachResult,NotificationVariant.LUMO_SUCCESS);
+            if(this.containerDialog != null){
+                this.containerDialog.close();
+            }
+        } catch (CoreRealmServiceRuntimeException e) {
+            throw new RuntimeException(e);
+        } catch (CoreRealmServiceEntityExploreException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void showPopupNotification(EntitiesOperationStatistics conceptionEntitiesAttributesRetrieveResult, NotificationVariant notificationVariant){
+        Notification notification = new Notification();
+        notification.addThemeVariants(notificationVariant);
+        Div text = new Div(new Text("概念类型 "+conceptionKindName+" 链接概念类型实体至时间流操作成功"));
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
+        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+        layout.setWidth(100, Unit.PERCENTAGE);
+        layout.setFlexGrow(1,text);
+        notification.add(layout);
+
+        VerticalLayout notificationMessageContainer = new VerticalLayout();
+        notificationMessageContainer.add(new Div(new Text("链接实体数: "+conceptionEntitiesAttributesRetrieveResult.getSuccessItemsCount())));
+        notificationMessageContainer.add(new Div(new Text("操作开始时间: "+conceptionEntitiesAttributesRetrieveResult.getStartTime())));
+        notificationMessageContainer.add(new Div(new Text("操作结束时间: "+conceptionEntitiesAttributesRetrieveResult.getFinishTime())));
+        notification.add(notificationMessageContainer);
+        notification.setDuration(3000);
+        notification.open();
     }
 }
