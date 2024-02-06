9e00e1ff467b352beb222e85a79bf4f00d66ba0b
wangyingchu <yingchuwang@gmail.com>
2024-02-06 15:47:06 +0800

    update for ConceptionEntitySpatialInfoView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/spatial/GeospatialEventDetailWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/spatial/GeospatialEventDetailWidget.java
@@ -1,10 +1,29 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity.spatial;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.GeospatialScaleEntity;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.GeospatialScaleEvent;
+import com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionEntity.common.AttributeValueInfoWidget;
+
+import java.util.List;
 
 public class GeospatialEventDetailWidget extends VerticalLayout {
 
-    public GeospatialEventDetailWidget(GeospatialScaleEvent geospatialScaleEvent, GeospatialScaleEntity geospatialScaleEntity){}
+    private GeospatialScaleEvent geospatialScaleEvent;
+    private GeospatialScaleEntity geospatialScaleEntity;
+
+    public GeospatialEventDetailWidget(GeospatialScaleEvent geospatialScaleEvent, GeospatialScaleEntity geospatialScaleEntity){
+        this.geospatialScaleEvent = geospatialScaleEvent;
+        this.geospatialScaleEntity = geospatialScaleEntity;
+
+        List<AttributeValue> attributeValueList = geospatialScaleEvent.getAttributes();
+        if(attributeValueList != null){
+            for(AttributeValue currentAttributeValue:attributeValueList){
+                AttributeValueInfoWidget attributeValueInfoWidget = new AttributeValueInfoWidget(currentAttributeValue);
+                add(attributeValueInfoWidget);
+            }
+        }
+    }
 }
