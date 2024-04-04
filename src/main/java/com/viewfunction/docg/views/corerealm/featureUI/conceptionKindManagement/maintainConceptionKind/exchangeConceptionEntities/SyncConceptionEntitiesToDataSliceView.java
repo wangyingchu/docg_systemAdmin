7d6dc661363d8a08def5e80c2c223265c6bb7793
wangychu <yingchuwang@163.com>
2024-04-04 11:36:39 +0800

    update for SyncConceptionEntitiesToDataSliceView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/exchangeConceptionEntities/SyncConceptionEntitiesToDataSliceView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionKind/exchangeConceptionEntities/SyncConceptionEntitiesToDataSliceView.java
@@ -1,18 +1,28 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.maintainConceptionKind.exchangeConceptionEntities;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import com.viewfunction.docg.dataCompute.computeServiceCore.exception.ComputeGridException;
+import com.viewfunction.docg.dataCompute.computeServiceCore.payload.DataComputeUnitMetaInfo;
+import com.viewfunction.docg.dataCompute.computeServiceCore.term.ComputeGrid;
+import com.viewfunction.docg.dataCompute.computeServiceCore.util.factory.ComputeGridTermFactory;
 import com.viewfunction.docg.element.commonComponent.FootprintMessageBar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class SyncConceptionEntitiesToDataSliceView extends VerticalLayout {
     private String conceptionKindName;
     private Dialog containerDialog;
+    private HorizontalLayout doesNotDetectDataGridInfoMessage;
 
     public SyncConceptionEntitiesToDataSliceView(String conceptionKindName){
         this.setWidthFull();
@@ -25,9 +35,39 @@ public class SyncConceptionEntitiesToDataSliceView extends VerticalLayout {
         footprintMessageVOList.add(new FootprintMessageBar.FootprintMessageVO(kindIcon, this.conceptionKindName));
         FootprintMessageBar entityInfoFootprintMessageBar = new FootprintMessageBar(footprintMessageVOList);
         add(entityInfoFootprintMessageBar);
+
+        doesNotDetectDataGridInfoMessage = new HorizontalLayout();
+        doesNotDetectDataGridInfoMessage.setSpacing(true);
+        doesNotDetectDataGridInfoMessage.setPadding(true);
+        doesNotDetectDataGridInfoMessage.setMargin(true);
+        doesNotDetectDataGridInfoMessage.setWidth(100,Unit.PERCENTAGE);
+        Icon messageLogo = new Icon(VaadinIcon.EXCLAMATION_CIRCLE);
+        messageLogo.getStyle()
+                .set("color","#ce0000").set("padding-right", "5px");
+        messageLogo.setSize("30px");
+        NativeLabel messageLabel = new NativeLabel(" 未检测到运行中的数据计算网格");
+        messageLabel.getStyle().set("font-size","var(--lumo-font-size-xl)").set("color","#ce0000");
+        doesNotDetectDataGridInfoMessage.add(messageLogo,messageLabel);
+        add(doesNotDetectDataGridInfoMessage);
+        doesNotDetectDataGridInfoMessage.setVisible(false);
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        checkComputeGridStatusInfo();
     }
 
     public void setContainerDialog(Dialog containerDialog) {
         this.containerDialog = containerDialog;
+    }
+
+    private void checkComputeGridStatusInfo(){
+        ComputeGrid targetComputeGrid = ComputeGridTermFactory.getComputeGrid();
+        try {
+            Set<DataComputeUnitMetaInfo> dataComputeUnitMetaInfoSet = targetComputeGrid.listDataComputeUnit();
+        } catch (ComputeGridException e) {
+            doesNotDetectDataGridInfoMessage.setVisible(true);
+        }
     }
 }
