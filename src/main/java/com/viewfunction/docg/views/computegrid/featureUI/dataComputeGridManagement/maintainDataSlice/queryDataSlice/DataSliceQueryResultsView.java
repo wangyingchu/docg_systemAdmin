7bf10471b4dbde6e6051d533a0808f06a775bc3b
wangyingchu <yingchuwang@gmail.com>
2024-04-22 18:12:14 +0800

    update for DataSliceQueryUI feature

---
+++ src/main/java/com/viewfunction/docg/views/computegrid/featureUI/dataComputeGridManagement/maintainDataSlice/queryDataSlice/DataSliceQueryResultsView.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.views.computegrid.featureUI.dataComputeGridManagement.maintainDataSlice.queryDataSlice;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class DataSliceQueryResultsView extends VerticalLayout {
+}
