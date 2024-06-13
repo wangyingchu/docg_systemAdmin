cda1f558b6973ce234cef8cf797717137f4fdf6a
wangyingchu <yingchuwang@gmail.com>
2024-06-13 13:53:57 +0800

    update Vaadin to version 24.4.1

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -36,3 +36,9 @@ import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
 import './theme-docg-pac.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
+
+import { Outlet } from 'react-router-dom';
+(window as any).Vaadin ??= {};
+(window as any).Vaadin.copilot ??= {};
+(window as any).Vaadin.copilot._ref ??= {};
+(window as any).Vaadin.copilot._ref.Outlet = Outlet;
