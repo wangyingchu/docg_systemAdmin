8e8e75483658fcf8522f4c52ac7a557c8fb48e2d
wangychu <yingchuwang@163.com>
2024-06-18 00:11:38 +0800

    update for ConceptionEntitiesRelationsChart new feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionEntitiesRelationsChart.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/queryConceptionKind/ConceptionEntitiesRelationsChart.java
@@ -1,6 +1,21 @@
 package com.viewfunction.docg.views.corerealm.featureUI.conceptionKindManagement.queryConceptionKind;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+@JavaScript("./visualization/feature/conceptionEntitiesRelationsChart-connector.js")
 public class ConceptionEntitiesRelationsChart extends VerticalLayout {
+
+    public ConceptionEntitiesRelationsChart() {
+        //link to download latest 3d-force-graph build js: https://unpkg.com/3d-force-graph
+        UI.getCurrent().getPage().addJavaScript("js/3d-force-graph/1.73.0/dist/three.js");
+        UI.getCurrent().getPage().addJavaScript("js/3d-force-graph/1.73.0/dist/three-spritetext.min.js");
+        UI.getCurrent().getPage().addJavaScript("js/3d-force-graph/1.73.0/dist/CSS2DRenderer.js");
+        UI.getCurrent().getPage().addJavaScript("js/3d-force-graph/1.73.0/dist/3d-force-graph.min.js");
+        this.setWidthFull();
+        this.setSpacing(false);
+        this.setMargin(false);
+        this.setPadding(false);
+    }
 }
