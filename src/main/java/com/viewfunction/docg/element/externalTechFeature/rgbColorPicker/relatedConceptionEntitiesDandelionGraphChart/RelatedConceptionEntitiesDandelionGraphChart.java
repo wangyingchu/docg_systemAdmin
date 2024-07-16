53da6957402e1cde15a1ebf4cc6eb4e569e4bc80
wangyingchu <yingchuwang@gmail.com>
2024-07-16 18:07:09 +0800

    add react tech integration example

---
+++ src/main/java/com/viewfunction/docg/element/externalTechFeature/rgbColorPicker/relatedConceptionEntitiesDandelionGraphChart/RelatedConceptionEntitiesDandelionGraphChart.java
@@ -0,0 +1,12 @@
+package com.viewfunction.docg.element.externalTechFeature.rgbColorPicker.relatedConceptionEntitiesDandelionGraphChart;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.react.ReactAdapterComponent;
+
+@NpmPackage(value = "react-force-graph", version = "1.44.4")
+@JsModule("./externalTech/flow/integration/feature/relatedConceptionEntitiesDandelionGraphChart/related-conceptionEntities-dandelionGraph-chart.tsx")
+@Tag("related-conceptionEntities-dandelionGraph-chart")
+public class RelatedConceptionEntitiesDandelionGraphChart extends ReactAdapterComponent {
+}
