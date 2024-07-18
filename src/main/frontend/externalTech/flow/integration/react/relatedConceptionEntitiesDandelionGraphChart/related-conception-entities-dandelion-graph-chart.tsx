7e25dce96604ac2e97a1107338aa59c0c4958ae1
wangyingchu <yingchuwang@gmail.com>
2024-07-18 17:23:31 +0800

    update for adding react-force-graph react component

--- src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
@@ -1,15 +1,10 @@
-import {RgbaColor, RgbaColorPicker} from "react-colorful";
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-force-graph';
 import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAdapter";
 import {ReactElement} from "react";
 
 class RelatedConceptionEntitiesDandelionGraphChartElement extends ReactAdapterElement {
     protected override render(hooks: RenderHooks): ReactElement | null {
-        const [color, setColor] =
-            hooks.useState<RgbaColor>("color"); // (2)
-
-        return <RgbaColorPicker
-            color={color}
-            onChange={setColor}
+        return <ForceGraph3D
         />; // (3)
     }
 }
--- src/main/java/com/viewfunction/docg/element/externalTechFeature/relatedConceptionEntitiesDandelionGraph/RelatedConceptionEntitiesDandelionGraphChart.java
+++ src/main/java/com/viewfunction/docg/element/externalTechFeature/relatedConceptionEntitiesDandelionGraph/RelatedConceptionEntitiesDandelionGraphChart.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.react.ReactAdapterComponent;
 
-@NpmPackage(value = "react-colorful", version = "5.6.1")
+@NpmPackage(value = "react-force-graph", version = "1.44.4")
 @JsModule("./externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx")
 @Tag("related-conception-entities-dandelion-graph-chart")
 public class RelatedConceptionEntitiesDandelionGraphChart extends ReactAdapterComponent {
