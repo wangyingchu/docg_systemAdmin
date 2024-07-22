852672f29e7f0b6ce1ee6a994fb99b42202dd7ee
wangyingchu <yingchuwang@gmail.com>
2024-07-22 17:20:49 +0800

    update for RelatedConceptionEntitiesDandelionGraphInfoView performance issue fixing

--- src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
@@ -1,11 +1,10 @@
 import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-force-graph';
+
 import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAdapter";
 import {ReactElement} from "react";
 
 class RelatedConceptionEntitiesDandelionGraphChartElement extends ReactAdapterElement {
     protected override render(hooks: RenderHooks): ReactElement | null {
-
-
         const xx = {
             nodes: [...Array(1000).keys()].map(i => ({id: i})),
             links: [...Array(1000).keys()]
@@ -16,15 +15,27 @@ class RelatedConceptionEntitiesDandelionGraphChartElement extends ReactAdapterEl
                 }))
         };
 
-        const backgroundColor = '#FFFFFF'
-
-
-        return <ForceGraph3D graphData={xx} backgroundColor={backgroundColor} width={800} height={800}
-        />; // (3)
+        return <ForceGraph3D graphData={xx}
+                             backgroundColor={'#FFFFFF'}
+                             width={800}
+                             height={800}
+                             nodeVal={node => {
+                                 return node.size;
+                             }}
+                             nodeRelSize={2}
+                             nodeResolution={100}
+                             nodeOpacity={0.85}
+                             linkOpacity={0.7}
+                             linkDirectionalArrowLength={4}
+                             linkCurvature={0.2}
+                             linkDirectionalArrowRelPos={0.5}
+                             linkWidth={0.3}
+                             linkThreeObjectExtend={true}
+        />;
     }
 }
 
 customElements.define(
     "related-conception-entities-dandelion-graph-chart",
     RelatedConceptionEntitiesDandelionGraphChartElement
-); // (4)
+);
