5ac43c834f1ee5440900ebe7e08efae441a757db
wangyingchu <yingchuwang@gmail.com>
2024-07-23 18:35:21 +0800

    update for RelatedConceptionEntitiesDandelionGraphInfoView performance issue fixing

--- src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx
@@ -2,6 +2,8 @@ import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-fo
 import * as THREE from 'three'
 import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAdapter";
 import {ReactElement} from "react";
+import SpriteText from 'three-spritetext';
+//import CSS2DRenderer from 'three-css2drenderer';
 
 class RelatedConceptionEntitiesDandelionGraphChartElement extends ReactAdapterElement {
     protected override render(hooks: RenderHooks): ReactElement | null {
@@ -15,37 +17,117 @@ class RelatedConceptionEntitiesDandelionGraphChartElement extends ReactAdapterEl
                 }))
         };
 
-        return <ForceGraph3D graphData={xx}
-                             //extraRenderers={[new THREE.CSS2DRenderer()]}
+        const GROUPS = 12;
+
+        let forceGraph3D = <ForceGraph3D graphData={xx}
+                                         backgroundColor={'#FFFFFF'}
+                                         nodeRelSize={6}
+                                         nodeResolution={15}
+                                         nodeOpacity={0.85}
+                                         linkOpacity={0.7}
+                                         linkDirectionalArrowLength={4}
+                                         linkCurvature={0.06}
+                                         linkDirectionalArrowRelPos={0.9}
+                                         linkWidth={0.8}
 
 
-                              linkThreeObjectExtend={true}
-
-                            linkThreeObject= {(link: { entityKind: any; source: any; target: any; }) => {
-                                // extend link with text sprite
-                                const sprite = new THREE.SpriteText(`${link.entityKind} : ${link.source} > ${link.target}`);
-                                sprite.color = 'black';
-                                sprite.textHeight = 1.5;
-                                return sprite;
-                            }}
 
 
-                             backgroundColor={'#CE0000'}
-                             width={800}
-                             height={800}
-                             nodeVal={node => {
-                                 return node.size;
-                             }}
-                             nodeRelSize={2}
-                             nodeResolution={100}
-                             nodeOpacity={0.85}
-                             linkOpacity={0.7}
-                             linkDirectionalArrowLength={4}
-                             linkCurvature={0.2}
-                             linkDirectionalArrowRelPos={0.5}
-                             linkWidth={0.3}
+                                     //    nodeAutoColorBy={d => d.id%GROUPS}
+                                     //    linkAutoColorBy={d => xx.nodes[d.source].id%GROUPS}
+
+
+            /*
+
+            */
+              /*
+                                         onNodeDragEnd={node => {
+                                             node.fx = node.x;
+                                             node.fy = node.y;
+                                             node.fz = node.z;
+                                         }}
+                                         */
+                                         linkThreeObjectExtend={true}
+                                        // extraRenderers={[new THREE.CSS2DRenderer()]}
+                                         linkThreeObject={(link: { entityKind: any; source: any; target: any; }) => {
+                                            // extend link with text sprite
+                                            const sprite = new SpriteText(`${link.entityKind} : ${link.source} > ${link.target}`);
+                                            sprite.color = 'black';
+                                            sprite.textHeight = 1.5;
+                                            return sprite;
+                                        }}
+
+
+                                         linkPositionUpdate={(sprite, { start, end }) => {
+                                             // @ts-ignore
+
+                                             const middlePos = Object.assign(...['x', 'y', 'z'].map(c => ({
+                                                 // @ts-ignore
+                                                 [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+                                             })));
+                                             // Position sprite
+                                             Object.assign(sprite.position, middlePos);
+                                         }}
+
+
+
+
+
+
+
+/*
+                                         linkThreeObject={link => {
+                                             // extend link with text sprite
+                                             const sprite = new SpriteText(`${link.entityKind} : ${link.source} > ${link.target}`);
+                                             sprite.color = 'black';
+                                             sprite.textHeight = 1.5;
+                                             return sprite;
+                                         }}
+*/
+
+
+
+
+
+
+
+
+
+            /*
+
+                                         //
+
+                                         linkThreeObjectExtend={true}
+
+                                        linkThreeObject= {(link: { entityKind: any; source: any; target: any; }) => {
+                                            // extend link with text sprite
+                                            const sprite = new THREE.SpriteText(`${link.entityKind} : ${link.source} > ${link.target}`);
+                                            sprite.color = 'black';
+                                            sprite.textHeight = 1.5;
+                                            return sprite;
+                                        }}
+
+
+                                         backgroundColor={'#CE0000'}
+                                         width={800}
+                                         height={800}
+                                         nodeVal={node => {
+                                             return node.size;
+                                         }}
+                                         nodeRelSize={2}
+                                         nodeResolution={100}
+                                         nodeOpacity={0.85}
+                                         linkOpacity={0.7}
+                                         linkDirectionalArrowLength={4}
+                                         linkCurvature={0.2}
+                                         linkDirectionalArrowRelPos={0.5}
+                                         linkWidth={0.3}
+              */
+
+
 
         />;
+        return forceGraph3D;
     }
 }
 
--- src/main/java/com/viewfunction/docg/element/externalTechFeature/relatedConceptionEntitiesDandelionGraph/RelatedConceptionEntitiesDandelionGraphChart.java
+++ src/main/java/com/viewfunction/docg/element/externalTechFeature/relatedConceptionEntitiesDandelionGraph/RelatedConceptionEntitiesDandelionGraphChart.java
@@ -6,7 +6,12 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.react.ReactAdapterComponent;
 
 @NpmPackage(value = "react-force-graph", version = "1.44.4")
-@NpmPackage(value = "three", version = "r166")
+@NpmPackage(value = "three", version = "0.166.1")
+//@NpmPackage(value = "three-css2drenderer", version = "1.0.0")
+@NpmPackage(value = "three-spritetext", version = "1.8.2")
+
+
+
 @JsModule("./externalTech/flow/integration/react/relatedConceptionEntitiesDandelionGraphChart/related-conception-entities-dandelion-graph-chart.tsx")
 @Tag("related-conception-entities-dandelion-graph-chart")
 public class RelatedConceptionEntitiesDandelionGraphChart extends ReactAdapterComponent {
