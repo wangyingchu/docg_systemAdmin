f4bc20f16c92996c5c983b03fe95d701cdbf1f87
wangychu <yingchuwang@163.com>
2024-07-28 23:08:30 +0800

    update for DataRelationDistributionChart_React feature

--- src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
@@ -4,14 +4,49 @@ import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAda
 import {ReactElement} from "react";
 
 class DataRelationDistributionChartElement extends ReactAdapterElement {
+    //https://github.com/plotly/react-cytoscapejs
     protected override render(hooks: RenderHooks): ReactElement | null {
+
         const elements = [
             { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
             { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
             { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
         ];
 
-        return <CytoscapeComponent elements={elements} style={ { width: '600px', height: '600px' } } />;
+
+
+
+
+        // @ts-ignore
+ /*       const elements={
+
+            CytoscapeComponent.normalizeElements({
+                nodes: [
+                    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+                    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } }
+                ],
+                edges: [
+                    {
+                        data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' }
+                    }
+                ]
+            })}
+*/
+        return <CytoscapeComponent
+
+
+
+
+            elements={elements}
+
+            style={ { width: '600px', height: '600px' } }
+
+
+
+
+
+
+        />;
     }
 }
 
