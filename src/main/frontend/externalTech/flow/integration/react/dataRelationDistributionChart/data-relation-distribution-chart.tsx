73ba072f920a50e5ea250a9ef9e13339c9401b51
wangyingchu <yingchuwang@gmail.com>
2024-07-29 14:34:42 +0800

    update for DataRelationDistributionChart_React feature

--- src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
@@ -1,26 +1,28 @@
 import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
+// @ts-ignore
+import Cytoscape from 'cytoscape';
+// @ts-ignore
+import COSEBilkent from 'cytoscape-cose-bilkent';
 import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAdapter";
 import {ReactElement} from "react";
 
+Cytoscape.use(COSEBilkent);
 class DataRelationDistributionChartElement extends ReactAdapterElement {
     //https://github.com/plotly/react-cytoscapejs
     protected override render(hooks: RenderHooks): ReactElement | null {
-
         const elements = [
             { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
             { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
             { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
         ];
 
+        const layout = { name: 'cose-bilkent' };
 
-
-
-
-        // @ts-ignore
- /*       const elements={
-
-            CytoscapeComponent.normalizeElements({
+        return <CytoscapeComponent
+            // @ts-ignore
+            cy={(cy) => { this.cy = cy }}
+            elements={CytoscapeComponent.normalizeElements({
                 nodes: [
                     { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
                     { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } }
@@ -31,21 +33,26 @@ class DataRelationDistributionChartElement extends ReactAdapterElement {
                     }
                 ]
             })}
-*/
-        return <CytoscapeComponent
-
-
-
-
-            elements={elements}
-
             style={ { width: '600px', height: '600px' } }
 
+            stylesheet={[
+                {
+                    selector: 'node',
+                    style: {
+                        width: 5,
+                        height: 5,
+                        shape: 'rectangle'
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        width: 2
+                    }
+                }
+            ]}
 
-
-
-
-
+            layout={layout}
         />;
     }
 }
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/DataRelationDistributionChart_React.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/coreRealmData/DataRelationDistributionChart_React.java
@@ -7,8 +7,11 @@ import com.vaadin.flow.component.react.ReactAdapterComponent;
 
 //https://www.npmjs.com/package/react-cytoscapejs
 //https://www.npmjs.com/package/cytoscape
+//https://www.npmjs.com/package/cytoscape-cose-bilkent
+
 @NpmPackage(value = "react-cytoscapejs", version = "2.0.0")
 @NpmPackage(value = "cytoscape", version = "3.30.1")
+@NpmPackage(value = "cytoscape-cose-bilkent", version = "4.1.0")
 @JsModule("./externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx")
 @Tag("data-relation-distribution-chart")
 public class DataRelationDistributionChart_React extends ReactAdapterComponent {
