c675d905e1b17ce637cac8d70debd951d2fd64ef
wangyingchu <yingchuwang@gmail.com>
2024-07-30 12:42:40 +0800

    update for DataRelationDistributionChart_React feature

--- src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
@@ -14,66 +14,77 @@ class DataRelationDistributionChartElement extends ReactAdapterElement {
         const [chartWidth, setWidth] = hooks.useState<number>("chartWidth");
         const [chartHeight, setHeight] = hooks.useState<number>("chartHeight");
         const [chartData, setChartData] = hooks.useState<any>("chartData");
-
-        const elements = [
-            { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-            { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-        ];
-
         const layout = { name: 'cose-bilkent' };
-
         const widthValueStr=''+chartWidth+'px';
         const heightValueStr=''+chartHeight+'px';
-
-
-
-
-
-
 
         return <CytoscapeComponent
             // @ts-ignore
             cy={(cy) => { this.cy = cy }}
-
-
-            simulateElements={CytoscapeComponent.normalizeElements({
-                nodes: [
-                    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-                    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } }
-                ],
-                edges: [
-                    {
-                        data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' }
-                    }
-                ]
-            })}
-
-            //elements={CytoscapeComponent.normalizeElements(chartData)}
-
             elements={CytoscapeComponent.normalizeElements(chartData)}
-
             style={
                 { width: widthValueStr, height: heightValueStr }
             }
-
             stylesheet={[
                 {
                     selector: 'node',
                     style: {
-                        width: 5,
-                        height: 5,
-                        shape: 'rectangle'
+                        'font-size': 1.1,
+                        'font-family': 'Montserrat',
+                        'font-weight': 'bold',
+                        'background-color': 'data(background_color)',
+                        'content': 'data(desc)',
+                        "text-wrap": "wrap",
+                        'text-valign': 'center',
+                        'color': '#333333',
+                        'shape': 'data(shape)',
+                        'text-outline-width': 0.15 ,
+                        'text-outline-color': '#EEE',
+                        'width': 'data(size)',
+                        'height': 'data(size)',
+                        'background-opacity': 1
                     }
                 },
                 {
                     selector: 'edge',
                     style: {
-                        width: 2
+                        'content': 'data(type)',
+                        'width': 'data(lineWidth)',
+                        'line-color': 'data(lineColor)',
+                        'source-arrow-color':'data(sourceArrowColor)',
+                        'target-arrow-color':'data(targetArrowColor)',
+                        'arrow-scale': 0.1,
+                        'line-style': 'data(lineStyle)',
+                        'curve-style': 'data(curveStyle)',
+                        'text-rotation': 'autorotate',
+                        'font-size': 0.8,
+                        'font-family': 'Montserrat',
+                        'color': '#666666',
+                        'target-arrow-shape': 'vee',
+                        'source-arrow-shape': 'tee',
+                        'line-opacity': 'data(lineOpacity)'
+                    }
+                },
+                {
+                    selector: 'node:selected',
+                    style: {
+                        'border-color': '#1E90CC',
+                        'border-width': "1px",
+                        'width':6,
+                        'height':6
+                    }
+                },
+                {
+                    selector: 'edge:selected',
+                    style: {
+                        'line-color': '#1E90CC',
+                        'width': 0.4,
+                        'source-arrow-color':'#1E90CC',
+                        'target-arrow-color':'#1E90CC',
+                        'color': '#333333'
                     }
                 }
             ]}
-
             layout={layout}
         />;
     }
