23987729dabf8a4bb795a4f5812c0bf7882923c1
wangyingchu <yingchuwang@gmail.com>
2024-07-31 10:52:55 +0800

    update for DataRelationDistributionChart_React feature

--- src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
+++ src/main/frontend/externalTech/flow/integration/react/dataRelationDistributionChart/data-relation-distribution-chart.tsx
@@ -5,7 +5,7 @@ import Cytoscape from 'cytoscape';
 // @ts-ignore
 import COSEBilkent from 'cytoscape-cose-bilkent';
 import {ReactAdapterElement, RenderHooks} from "Frontend/generated/flow/ReactAdapter";
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useRef} from "react";
 
 Cytoscape.use(COSEBilkent);
 class DataRelationDistributionChartElement extends ReactAdapterElement {
@@ -17,10 +17,83 @@ class DataRelationDistributionChartElement extends ReactAdapterElement {
         const layout = { name: 'cose-bilkent' };
         const widthValueStr=''+chartWidth+'px';
         const heightValueStr=''+chartHeight+'px';
+        const fgRef = useRef();
+
+        // @ts-ignore
+        const setListeners = (cy) => {
+            // @ts-ignore
+            cy.on('select', 'node', function(evt){
+                let node = evt.target;
+                node.connectedEdges();
+                // @ts-ignore
+                cy.style()
+                    .selector(node.connectedEdges())
+                    .style({
+                        'line-color': '#1E90CC',
+                        'width': 0.4,
+                        'source-arrow-color':'#1E90CC',
+                        'target-arrow-color':'#1E90CC',
+                        'color': '#333333'
+                    })
+                    .update();
+            });
+
+            // @ts-ignore
+            cy.on('unselect', 'node', function(evt){
+                let node = evt.target;
+                cy.style()
+                    .selector(node.connectedEdges())
+                    .style({
+                        'content': 'data(type)',
+                        'width': 'data(lineWidth)',
+                        'line-color': 'data(lineColor)',
+                        'source-arrow-color':'data(sourceArrowColor)',
+                        'target-arrow-color':'data(targetArrowColor)',
+                        'arrow-scale': 0.1,
+                        'line-style': 'data(lineStyle)',
+                        'curve-style': 'data(curveStyle)',
+                        'text-rotation': 'autorotate',
+                        'font-size': 0.8,
+                        'color': '#666666',
+                        'target-arrow-shape': 'vee',
+                        'source-arrow-shape': 'tee',
+                        'line-opacity': 'data(lineOpacity)'
+                    })
+                    .update();
+            });
+
+            // @ts-ignore
+            cy.on('select', 'edge', function(evt){
+                let edge = evt.target;
+                cy.style()
+                    .selector(edge.connectedNodes())
+                    .style({
+                        'border-color': '#1E90CC',
+                        'border-width': "1px",
+                        'width':6,'height':6
+                    })
+                    .update();
+            });
+
+            // @ts-ignore
+            cy.on('unselect', 'edge', function(evt){
+                let edge = evt.target;
+                cy.style()
+                    .selector(edge.connectedNodes())
+                    .style({
+                        'border-width': "0px",
+                    })
+                    .update();
+            });
+        };
 
         return <CytoscapeComponent
+            //need set ref for useEffect function
+            ref={fgRef}
             // @ts-ignore
-            cy={(cy) => { this.cy = cy }}
+            cy={(cy) => {
+                setListeners(cy);
+            }}
             elements={CytoscapeComponent.normalizeElements(chartData)}
             style={
                 { width: widthValueStr, height: heightValueStr }
