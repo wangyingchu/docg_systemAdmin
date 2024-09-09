42688e784e19e7a6911bd64ef9904619366aa7b4
wangychu <yingchuwang@163.com>
2024-09-09 23:46:36 +0800

    update for ConceptionEntityExpandGraphView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/pathAnalysis/RelationKindMatchLogicWidget.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/pathAnalysis/RelationKindMatchLogicWidget.java
@@ -1,8 +1,14 @@
 package com.viewfunction.docg.views.corerealm.featureUI.commonUIComponent.pathAnalysis;
 
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.combobox.ComboBoxVariant;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.RelationDirection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RelationKindMatchLogicWidget extends HorizontalLayout {
 
@@ -14,7 +20,25 @@ public class RelationKindMatchLogicWidget extends HorizontalLayout {
 
         NativeLabel defaultRelationDirectionFilterText = new NativeLabel("默认全局关系方向:");
         defaultRelationDirectionFilterText.getStyle().set("font-size","0.7rem").set("color","var(--lumo-contrast-80pct)");
+        defaultRelationDirectionFilterText.addClassNames("text-xs","font-semibold","text-secondary");
         add(defaultRelationDirectionFilterText);
+        this.setVerticalComponentAlignment(Alignment.CENTER,defaultRelationDirectionFilterText);
+
+        ComboBox<RelationDirection> provinceValueTextField = new ComboBox();
+        provinceValueTextField.addThemeVariants(ComboBoxVariant.LUMO_SMALL,ComboBoxVariant.LUMO_ALIGN_RIGHT);
+        List<RelationDirection> relationDirectionList = new ArrayList<>();
+        relationDirectionList.add(RelationDirection.FROM);
+        relationDirectionList.add(RelationDirection.TO);
+        relationDirectionList.add(RelationDirection.TWO_WAY);
+        provinceValueTextField.setItems(relationDirectionList);
+
+
+
+
+        provinceValueTextField.addThemeVariants(ComboBoxVariant.LUMO_SMALL);
+        provinceValueTextField.setWidth(100, Unit.PIXELS);
+
+        add(provinceValueTextField);
 
     }
 
