58394a058650b5c6f77ae21c3dab3b13d0159e17
wangychu <yingchuwang@163.com>
2024-09-16 12:59:51 +0800

    update for ConceptionEntityPathTravelableResultView feature

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/pathAnalysis/ConceptionEntityPathTravelableResultView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/commonUIComponent/pathAnalysis/ConceptionEntityPathTravelableResultView.java
@@ -4,8 +4,15 @@ import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.structure.EntitiesPath;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import com.viewfunction.docg.element.eventHandling.ConceptionEntityExpandPathEvent;
 import com.viewfunction.docg.util.ResourceHolder;
+
+import java.util.List;
 
 public class ConceptionEntityPathTravelableResultView extends VerticalLayout implements ConceptionEntityExpandPathEvent.ConceptionEntityExpandPathListener{
 
@@ -40,8 +47,8 @@ public class ConceptionEntityPathTravelableResultView extends VerticalLayout imp
 
     @Override
     public void receivedConceptionEntityExpandPathEvent(ConceptionEntityExpandPathEvent event) {
-
-
+        String conceptionKind = event.getConceptionKind();
+        String conceptionEntityUID = event.getConceptionEntityUID();
 
         System.out.println(event.getConceptionEntityUID());
         System.out.println(event.getConceptionKind());
@@ -51,8 +58,17 @@ public class ConceptionEntityPathTravelableResultView extends VerticalLayout imp
         System.out.println(event.getMaxJump());
         System.out.println(event.getMinJump());
         System.out.println(event.isContainsSelf());
-        System.out.println(event.getRelationKindMatchLogics());
+        System.out.println(event.getConceptionKindMatchLogics());
 
-
+        CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+        ConceptionKind targetConception = coreRealm.getConceptionKind(conceptionKind);
+        if(targetConception != null){
+            ConceptionEntity targetConceptionEntity = targetConception.getEntityByUID(conceptionEntityUID);
+            if(targetConceptionEntity != null){
+                List<EntitiesPath> entitiesPathList =  targetConceptionEntity.expandPath(event.getRelationKindMatchLogics(),event.getDefaultDirectionForNoneRelationKindMatch(),event.getConceptionKindMatchLogics(),event.getMinJump(),event.getMaxJump());
+                System.out.println(entitiesPathList);
+                System.out.println(entitiesPathList.size());
+            }
+        }
     }
 }
