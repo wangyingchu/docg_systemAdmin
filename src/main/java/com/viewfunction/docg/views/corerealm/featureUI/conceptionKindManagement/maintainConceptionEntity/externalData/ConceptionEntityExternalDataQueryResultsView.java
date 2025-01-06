118ca7e8fa499879e4c90b82652d41805664c243
wangychu <yingchuwang@163.com>
2025-01-06 10:10:58 +0800

    Add external attributes access view to ConceptionEntityIntegratedInfoView

--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/externalData/ConceptionEntityExternalDataQueryResultsView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/conceptionKindManagement/maintainConceptionEntity/externalData/ConceptionEntityExternalDataQueryResultsView.java
@@ -26,11 +26,9 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query.QueryPara
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.feature.GeospatialScaleCalculable;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntitiesAttributesRetrieveResult;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntityExternalAttributesValueRetrieveResult;
 import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntityValue;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeKind;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributesViewKind;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.*;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import com.viewfunction.docg.element.commonComponent.*;
@@ -141,83 +139,81 @@ public class ConceptionEntityExternalDataQueryResultsView extends VerticalLayout
             }
         });
 
-        MenuItem analyzeDataSubMenu = subMenu.addItem(LineAwesomeIconsSvg.BONG_SOLID.create());
-        analyzeDataSubMenu.add(" 数据分析");
-
-        MenuItem displayGISInfo = analyzeDataSubMenu.getSubMenu().addItem(VaadinIcon.GLOBE.create());
-        displayGISInfo.add("随机抽样显示地理空间属性");
-
-        MenuItem globalGISInfo = displayGISInfo.getSubMenu().addItem(VaadinIcon.GLOBE_WIRE.create());
-        globalGISInfo.add("全球坐标系地理空间信息");
-
-        MenuItem countryGISInfo = displayGISInfo.getSubMenu().addItem(VaadinIcon.LOCATION_ARROW_CIRCLE.create());
-        countryGISInfo.add("国家坐标系地理空间信息");
-
-        MenuItem localGISInfo = displayGISInfo.getSubMenu().addItem(VaadinIcon.HOME.create());
-        localGISInfo.add("本地坐标系地理空间信息");
-
-        globalGISInfo.addClickListener(new ComponentEventListener<ClickEvent<MenuItem>>() {
-            @Override
-            public void onComponentEvent(ClickEvent<MenuItem> menuItemClickEvent) {
-                showEntitiesGISInfo(GeospatialScaleCalculable.SpatialScaleLevel.Global);
-            }
-        });
-
-        countryGISInfo.addClickListener(new ComponentEventListener<ClickEvent<MenuItem>>() {
-            @Override
-            public void onComponentEvent(ClickEvent<MenuItem> menuItemClickEvent) {
-                showEntitiesGISInfo(GeospatialScaleCalculable.SpatialScaleLevel.Country);
-            }
-        });
-
-        localGISInfo.addClickListener(new ComponentEventListener<ClickEvent<MenuItem>>() {
-            @Override
-            public void onComponentEvent(ClickEvent<MenuItem> menuItemClickEvent) {
-                showEntitiesGISInfo(GeospatialScaleCalculable.SpatialScaleLevel.Local);
-            }
-        });
-
         queryResultGrid = new Grid<>();
         queryResultGrid.setWidth(100, Unit.PERCENTAGE);
         queryResultGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
         queryResultGrid.addThemeVariants(GridVariant.LUMO_COLUMN_BORDERS);
-        queryResultGrid.addColumn(new ValueProvider<ConceptionEntityValue, Object>() {
-            @Override
-            public Object apply(ConceptionEntityValue conceptionEntityValue) {
-                return conceptionEntityValue.getEntityAttributesValue().get(_rowIndexPropertyName);
-            }
-        }).setHeader("").setHeader("IDX").setKey("idx").setFlexGrow(0).setWidth("75px").setResizable(false);
-        queryResultGrid.addComponentColumn(new ConceptionEntityActionButtonsValueProvider()).setHeader("操作").setKey("idx_0").setFlexGrow(0).setWidth("120px").setResizable(false);
-
-
-        queryResultGrid.addColumn(ConceptionEntityValue::getConceptionEntityUID).setHeader(" EntityUID").setKey("idx_1").setFlexGrow(1).setWidth("150px").setResizable(false);
-
-
-
 
         for (AttributeKind currentAttributeKind : this.attributesViewKind.getContainsAttributeKinds()) {
-            queryResultGrid.addColumn(ConceptionEntityValue::getConceptionEntityUID).setHeader(" "+currentAttributeKind.getAttributeKindName()).setKey("idx_"+currentAttributeKind.getAttributeKindUID()).setResizable(true);
+            String columnKey = "idx_" + currentAttributeKind.getAttributeKindUID();
+            String columnName = " "+currentAttributeKind.getAttributeKindName();
+            queryResultGrid.addColumn(ConceptionEntityValue::getConceptionEntityUID).setHeader(columnName).setKey(columnKey).setResizable(true);
+            GridColumnHeader gridColumnHeader_idx = new GridColumnHeader(LineAwesomeIconsSvg.CIRCLE.create(),columnName);
+            queryResultGrid.getColumnByKey(columnKey).setHeader(gridColumnHeader_idx).setSortable(false);
         }
-
-
-
-
-
-
-        GridColumnHeader gridColumnHeader_idx = new GridColumnHeader(VaadinIcon.LIST_OL,"");
-        queryResultGrid.getColumnByKey("idx").setHeader(gridColumnHeader_idx).setSortable(false);
-        GridColumnHeader gridColumnHeader_idx1 = new GridColumnHeader(VaadinIcon.WRENCH,"操作");
-        queryResultGrid.getColumnByKey("idx_0").setHeader(gridColumnHeader_idx1).setSortable(false);
-        GridColumnHeader gridColumnHeader_idx0 = new GridColumnHeader(VaadinIcon.KEY_O,"概念实体UID");
-        queryResultGrid.getColumnByKey("idx_1").setHeader(gridColumnHeader_idx0).setSortable(false).setResizable(true);
         add(queryResultGrid);
 
         this.currentRowKeyList = new ArrayList<>();
     }
 
+    public void queryExternalValueAttributesViewData(QueryParameters queryParameters){
+        CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+        coreRealm.openGlobalSession();
+        ConceptionKind targetConceptionKind = coreRealm.getConceptionKind(this.conceptionKindName);
+        AttributesViewKind targetAttributesViewKind = coreRealm.getAttributesViewKind(this.attributesViewKind.getAttributesViewKindUID());
+        if(targetConceptionKind != null && targetAttributesViewKind != null){
+            ConceptionEntity targetConceptionEntity = targetConceptionKind.getEntityByUID(this.conceptionEntityUID);
+
+            if(targetConceptionEntity != null){
+                try {
+                    ConceptionEntityExternalAttributesValueRetrieveResult conceptionEntityExternalAttributesValueRetrieveResult
+                            = targetConceptionEntity.getEntityExternalAttributesValues(targetAttributesViewKind,queryParameters);
+                    if(conceptionEntityExternalAttributesValueRetrieveResult != null ) {
+                        showPopupNotification(conceptionEntityExternalAttributesValueRetrieveResult, NotificationVariant.LUMO_SUCCESS);
+                        Date startDateTime = conceptionEntityExternalAttributesValueRetrieveResult.getStartTime();
+                        ZonedDateTime startZonedDateTime = ZonedDateTime.ofInstant(startDateTime.toInstant(), id);
+                        String startTimeStr = startZonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime((FormatStyle.MEDIUM)));
+                        startTimeDisplayItem.updateDisplayValue(startTimeStr);
+                        Date finishDateTime = conceptionEntityExternalAttributesValueRetrieveResult.getFinishTime();
+                        ZonedDateTime finishZonedDateTime = ZonedDateTime.ofInstant(finishDateTime.toInstant(), id);
+                        String finishTimeStr = finishZonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime((FormatStyle.MEDIUM)));
+                        finishTimeDisplayItem.updateDisplayValue(finishTimeStr);
+                        dataCountDisplayItem.updateDisplayValue("" + numberFormat.format(conceptionEntityExternalAttributesValueRetrieveResult.getResultRowsCount()));
+
+                        /*
+                        List<ConceptionEntityValue> conceptionEntityValueList = conceptionEntitiesAttributesRetrieveResult.getConceptionEntityValues();
+                        for (int i = 0; i < conceptionEntityValueList.size(); i++) {
+                            ConceptionEntityValue currentConceptionEntityValue = conceptionEntityValueList.get(i);
+                            currentConceptionEntityValue.getEntityAttributesValue().put(_rowIndexPropertyName, i + 1);
+                        }
+                        if (attributesList != null && attributesList.size() > 0) {
+                            for (String currentProperty : attributesList) {
+                                if (!currentProperty.equals(_rowIndexPropertyName)) {
+                                    queryResultGrid.addColumn(new ValueProvider<ConceptionEntityValue, Object>() {
+                                        @Override
+                                        public Object apply(ConceptionEntityValue conceptionEntityValue) {
+                                            return conceptionEntityValue.getEntityAttributesValue().get(currentProperty);
+                                        }
+                                    }).setHeader(" " + currentProperty).setKey(currentProperty + "_KEY");
+                                    queryResultGrid.getColumnByKey(currentProperty + "_KEY").setSortable(true).setResizable(true);
+                                }
+
+                                this.currentRowKeyList.add(currentProperty + "_KEY");
+                            }
+                        }
+                        queryResultGrid.setItems(conceptionEntityValueList);
+                        */
 
 
-    public void queryExternalValueAttributesViewData(QueryParameters queryParameters){}
+                        queryResultOperationMenuBar.setEnabled(true);
+                    }
+                } catch (CoreRealmServiceEntityExploreException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        coreRealm.closeGlobalSession();
+    }
 
 
 
@@ -314,88 +310,13 @@ public class ConceptionEntityExternalDataQueryResultsView extends VerticalLayout
 
     @Override
     protected void onDetach(DetachEvent detachEvent) {
-        // Listener needs to be eventually removed in order to avoid resource leak
         super.onDetach(detachEvent);
     }
 
-    /*
-    @Override
-    public void receivedConceptionEntityDeletedEvent(ConceptionEntityDeletedEvent event) {
-        List<String> deletedEntityAllConceptionKinds = event.getEntityAllConceptionKindNames();
-        if(deletedEntityAllConceptionKinds.contains(conceptionKindName)){
-            String conceptionEntityUID = event.getConceptionEntityUID();
-            ListDataProvider dataProvider=(ListDataProvider)queryResultGrid.getDataProvider();
-            ConceptionEntityValue conceptionEntityValueToDelete = null;
-            Collection<ConceptionEntityValue> conceptionEntityValueList = dataProvider.getItems();
-            for(ConceptionEntityValue currentConceptionEntityValue:conceptionEntityValueList){
-                if(currentConceptionEntityValue.getConceptionEntityUID().equals(conceptionEntityUID)){
-                    conceptionEntityValueToDelete = currentConceptionEntityValue;
-                    break;
-                }
-            }
-            if(conceptionEntityValueToDelete != null){
-                dataProvider.getItems().remove(conceptionEntityValueToDelete);
-                dataProvider.refreshAll();
-            }
-        }
-    }
-    */
-    private class ConceptionEntityActionButtonsValueProvider implements ValueProvider<ConceptionEntityValue,HorizontalLayout>{
-        @Override
-        public HorizontalLayout apply(ConceptionEntityValue conceptionEntityValue) {
-            HorizontalLayout actionButtonContainerLayout = new HorizontalLayout();
-            actionButtonContainerLayout.setMargin(false);
-            actionButtonContainerLayout.setSpacing(false);
-            Button showDetailButton = new Button();
-            showDetailButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-            showDetailButton.setIcon(VaadinIcon.EYE.create());
-            showDetailButton.setTooltipText("显示概念实体详情");
-            actionButtonContainerLayout.add(showDetailButton);
-            showDetailButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
-                @Override
-                public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
-                    if(conceptionEntityValue != null){
-                        //renderConceptionEntityUI(conceptionEntityValue);
-                    }
-                }
-            });
-
-            Button addToProcessListButton = new Button();
-            addToProcessListButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-            addToProcessListButton.setIcon(VaadinIcon.INBOX.create());
-            addToProcessListButton.setTooltipText("加入待处理数据列表");
-            actionButtonContainerLayout.add(addToProcessListButton);
-            addToProcessListButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
-                @Override
-                public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
-                    if(conceptionEntityValue != null){
-                        //addConceptionEntityToProcessingList(conceptionEntityValue);
-                    }
-                }
-            });
-
-            Button deleteButton = new Button();
-            deleteButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-            deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR );
-            deleteButton.setIcon(VaadinIcon.TRASH.create());
-            deleteButton.setTooltipText("删除概念实体");
-            actionButtonContainerLayout.add(deleteButton);
-            deleteButton.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
-                @Override
-                public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
-                    if(conceptionEntityValue != null){
-                        //deleteConceptionEntity(conceptionEntityValue);
-                    }
-                }
-            });
-            return actionButtonContainerLayout;
-        }
-    }
-
-    private void showPopupNotification(ConceptionEntitiesAttributesRetrieveResult conceptionEntitiesAttributesRetrieveResult, NotificationVariant notificationVariant){
+    private void showPopupNotification(ConceptionEntityExternalAttributesValueRetrieveResult conceptionEntityExternalAttributesValueRetrieveResult, NotificationVariant notificationVariant){
         Notification notification = new Notification();
         notification.addThemeVariants(notificationVariant);
-        Div text = new Div(new Text("概念类型 "+conceptionKindName+" 实例查询操作成功"));
+        Div text = new Div(new Text("概念实体 "+this.conceptionEntityUID+" 外部属性视图数据查询操作成功"));
         Button closeButton = new Button(new Icon("lumo", "cross"));
         closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
         closeButton.addClickListener(event -> {
@@ -407,15 +328,13 @@ public class ConceptionEntityExternalDataQueryResultsView extends VerticalLayout
         notification.add(layout);
 
         VerticalLayout notificationMessageContainer = new VerticalLayout();
-        notificationMessageContainer.add(new Div(new Text("查询返回实体数: "+conceptionEntitiesAttributesRetrieveResult.getOperationStatistics().getResultEntitiesCount())));
-        notificationMessageContainer.add(new Div(new Text("操作开始时间: "+conceptionEntitiesAttributesRetrieveResult.getOperationStatistics().getStartTime())));
-        notificationMessageContainer.add(new Div(new Text("操作结束时间: "+conceptionEntitiesAttributesRetrieveResult.getOperationStatistics().getFinishTime())));
+        notificationMessageContainer.add(new Div(new Text("查询返回数据数: "+conceptionEntityExternalAttributesValueRetrieveResult.getResultRowsCount())));
+        notificationMessageContainer.add(new Div(new Text("操作开始时间: "+conceptionEntityExternalAttributesValueRetrieveResult.getStartTime())));
+        notificationMessageContainer.add(new Div(new Text("操作结束时间: "+conceptionEntityExternalAttributesValueRetrieveResult.getFinishTime())));
         notification.add(notificationMessageContainer);
         notification.setDuration(3000);
         notification.open();
     }
-
-
 
     private void exportCSVQueryResult(){
         DownloadCSVFormatQueryResultsView downloadCSVFormatQueryResultsView = new DownloadCSVFormatQueryResultsView(this.conceptionKindName,this.lastConceptionEntitiesAttributesRetrieveResult,this.lastQueryAttributesList,500);
@@ -424,16 +343,6 @@ public class ConceptionEntityExternalDataQueryResultsView extends VerticalLayout
         fixSizeWindow.setWindowContent(downloadCSVFormatQueryResultsView);
         fixSizeWindow.setModel(true);
         downloadCSVFormatQueryResultsView.setContainerDialog(fixSizeWindow);
-        fixSizeWindow.show();
-    }
-
-    private void exportArrowQueryResult(){
-        DownloadArrowFormatQueryResultsView downloadArrowFormatQueryResultsView = new DownloadArrowFormatQueryResultsView(this.conceptionKindName,this.lastConceptionEntitiesAttributesRetrieveResult,this.lastQueryAttributesList,500);
-        FixSizeWindow fixSizeWindow = new FixSizeWindow(new Icon(VaadinIcon.DOWNLOAD),"导出 ARROW 格式概念类型实体数据查询结果",null,true,550,290,false);
-        fixSizeWindow.disableCloseButton();
-        fixSizeWindow.setWindowContent(downloadArrowFormatQueryResultsView);
-        fixSizeWindow.setModel(true);
-        downloadArrowFormatQueryResultsView.setContainerDialog(fixSizeWindow);
         fixSizeWindow.show();
     }
 
@@ -446,29 +355,4 @@ public class ConceptionEntityExternalDataQueryResultsView extends VerticalLayout
         downloadExcelFormatQueryResultsView.setContainerDialog(fixSizeWindow);
         fixSizeWindow.show();
     }
-
-    private void showEntitiesGISInfo(GeospatialScaleCalculable.SpatialScaleLevel spatialScaleLevel){
-        Icon windowTitleIcon = null;
-        String windowTitleTxt = this.conceptionKindName + " 概念实体 ("+ this.numberFormat.format(this.lastConceptionEntitiesAttributesRetrieveResult.getConceptionEntityValues().size())+") ";
-        switch(spatialScaleLevel){
-            case Global :
-                windowTitleIcon = VaadinIcon.GLOBE_WIRE.create();
-                windowTitleTxt = windowTitleTxt + "全球坐标系空间数据展示";
-                break;
-            case Country:
-                windowTitleIcon = VaadinIcon.LOCATION_ARROW_CIRCLE.create();
-                windowTitleTxt = windowTitleTxt + "国家坐标系空间数据展示";
-                break;
-            case Local:
-                windowTitleIcon = VaadinIcon.HOME.create();
-                windowTitleTxt = windowTitleTxt + "本地坐标系空间数据展示";
-                break;
-        }
-
-        FullScreenWindow fullScreenWindow = new FullScreenWindow(windowTitleIcon,windowTitleTxt,null,null,true);
-        ConceptionEntitiesGeospatialInfoAnalysisView conceptionEntitiesGeospatialInfoAnalysisView = new ConceptionEntitiesGeospatialInfoAnalysisView(this.conceptionKindName,spatialScaleLevel,this.lastConceptionEntitiesAttributesRetrieveResult);
-        fullScreenWindow.setWindowContent(conceptionEntitiesGeospatialInfoAnalysisView);
-        fullScreenWindow.show();
-    }
-
 }
