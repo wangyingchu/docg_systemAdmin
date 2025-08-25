6d1d8421b2535bde81d091ac4ccd8ee36e396b99
wangyingchu <yingchuwang@gmail.com>
2025-08-25 17:48:13 +0800

    Add real-time data widgets for intelligent analysis view

---
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/intelligentAnalysis/ConceptionDataCorrelationRealtimeInfoWidget.java
@@ -0,0 +1,24 @@
+package com.viewfunction.docg.views.corerealm.featureUI.intelligentAnalysis;
+
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class ConceptionDataCorrelationRealtimeInfoWidget extends VerticalLayout {
+    public ConceptionDataCorrelationRealtimeInfoWidget(){
+        this.getStyle().set("background-color", "white");
+        Span name = new Span("Sophia Williams");
+        Span email = new Span("sophia.williams@company.com");
+        Span phone = new Span("(501) 555-9128");
+
+        VerticalLayout content = new VerticalLayout(name, email, phone);
+        content.setSpacing(false);
+        content.setPadding(false);
+
+        Details details = new Details("Contact information", content);
+        details.setOpened(true);
+
+        add(details);
+    }
+
+}
--- src/main/java/com/viewfunction/docg/views/corerealm/featureUI/intelligentAnalysis/IntelligentAnalysisView.java
+++ src/main/java/com/viewfunction/docg/views/corerealm/featureUI/intelligentAnalysis/IntelligentAnalysisView.java
@@ -1,6 +1,7 @@
 package com.viewfunction.docg.views.corerealm.featureUI.intelligentAnalysis;
 
 import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.details.Details;
@@ -12,19 +13,32 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.operator.SystemMaintenanceOperator;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeSystemInfo;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionKindCorrelationInfo;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.EntityStatisticsInfo;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import com.viewfunction.docg.element.commonComponent.SectionActionBar;
 import com.viewfunction.docg.element.commonComponent.SectionWallContainer;
 import com.viewfunction.docg.element.commonComponent.SectionWallTitle;
 import com.viewfunction.docg.element.commonComponent.lineAwesomeIcon.LineAwesomeIconsSvg;
 import com.viewfunction.docg.views.corerealm.featureUI.coreRealmData.SystemRuntimeInfoWidget;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Route("coreRealmIntelligentAnalysis/")
-public class IntelligentAnalysisView  extends VerticalLayout {
+public class IntelligentAnalysisView extends VerticalLayout {
 
     private VerticalLayout leftSideContainerLayout;
+    private ConceptionDataRealtimeInfoWidget conceptionDataRealtimeInfoWidget;
+    private RelationDataRealtimeInfoWidget relationDataRealtimeInfoWidget;
+    private ConceptionDataCorrelationRealtimeInfoWidget conceptionDataCorrelationRealtimeInfoWidget;
 
     public IntelligentAnalysisView() {
         HorizontalLayout mainContainerLayout = new HorizontalLayout();
@@ -40,13 +54,6 @@ public class IntelligentAnalysisView  extends VerticalLayout {
         leftSideContainerLayout.setWidth(500, Unit.PIXELS);
         leftSideContainerLayout.getStyle().set("border-right", "1px solid var(--lumo-contrast-20pct)");
 
-
-
-
-
-
-
-
         List<Component> sectionAction2ComponentsList = new ArrayList<>();
         SectionActionBar sectionActionBar2 = new SectionActionBar(LineAwesomeIconsSvg.CHART_LINE_SOLID.create(),"全域数据实时分布",sectionAction2ComponentsList);
         leftSideContainerLayout.add(sectionActionBar2);
@@ -55,44 +62,88 @@ public class IntelligentAnalysisView  extends VerticalLayout {
         SystemRuntimeInfoWidget systemRuntimeInfoWidget = new SystemRuntimeInfoWidget();
         leftSideContainerLayout.add(systemRuntimeInfoWidget);
 
+        conceptionDataRealtimeInfoWidget = new ConceptionDataRealtimeInfoWidget();
         Icon conceptionKindInfoTitleIcon = new Icon(VaadinIcon.CUBE);
         conceptionKindInfoTitleIcon.setSize("18px");
         NativeLabel conceptionKindInfoTitleLabel = new NativeLabel("ConceptionKind(概念类型) 数据分布");
         conceptionKindInfoTitleLabel.getStyle().set("font-size","var(--lumo-font-size-m)");
         SectionWallTitle conceptionKindInfoSectionWallTitle = new SectionWallTitle(conceptionKindInfoTitleIcon,conceptionKindInfoTitleLabel);
-        SectionWallContainer conceptionKindInfoSectionWallContainer = new SectionWallContainer(conceptionKindInfoSectionWallTitle,new VerticalLayout());
+        SectionWallContainer conceptionKindInfoSectionWallContainer = new SectionWallContainer(conceptionKindInfoSectionWallTitle,conceptionDataRealtimeInfoWidget);
         leftSideContainerLayout.add(conceptionKindInfoSectionWallContainer);
 
+        relationDataRealtimeInfoWidget = new RelationDataRealtimeInfoWidget();
         Icon relationKindInfoTitleIcon = new Icon(VaadinIcon.CONNECT_O);
         relationKindInfoTitleIcon.setSize("18px");
         NativeLabel relationKindInfoTitleLabel = new NativeLabel("RelationKind(关系类型) 数据分布");
         relationKindInfoTitleLabel.getStyle().set("font-size","var(--lumo-font-size-m)");
         SectionWallTitle relationKindInfoSectionWallTitle = new SectionWallTitle(relationKindInfoTitleIcon,relationKindInfoTitleLabel);
-        SectionWallContainer relationKindInfoSectionWallContainer = new SectionWallContainer(relationKindInfoSectionWallTitle,new VerticalLayout());
+        SectionWallContainer relationKindInfoSectionWallContainer = new SectionWallContainer(relationKindInfoSectionWallTitle,relationDataRealtimeInfoWidget);
         leftSideContainerLayout.add(relationKindInfoSectionWallContainer);
 
+        conceptionDataCorrelationRealtimeInfoWidget = new ConceptionDataCorrelationRealtimeInfoWidget();
         Icon dataRelationInfoTitleIcon = FontAwesome.Solid.CODE_FORK.create();
         relationKindInfoTitleIcon.setSize("18px");
         NativeLabel dataRelationInfoTitleLabel = new NativeLabel("全域数据实时关联 数据分布");
         dataRelationInfoTitleLabel.getStyle().set("font-size","var(--lumo-font-size-m)");
         SectionWallTitle dataRelationInfoSectionWallTitle = new SectionWallTitle(dataRelationInfoTitleIcon,dataRelationInfoTitleLabel);
-        SectionWallContainer dataRelationInfoSectionWallContainer = new SectionWallContainer(dataRelationInfoSectionWallTitle,new VerticalLayout());
+        SectionWallContainer dataRelationInfoSectionWallContainer = new SectionWallContainer(dataRelationInfoSectionWallTitle,conceptionDataCorrelationRealtimeInfoWidget);
         leftSideContainerLayout.add(dataRelationInfoSectionWallContainer);
+    }
 
-        Span name = new Span("Sophia Williams");
-        Span email = new Span("sophia.williams@company.com");
-        Span phone = new Span("(501) 555-9128");
-
-        VerticalLayout content = new VerticalLayout(name, email, phone);
-        content.setSpacing(false);
-        content.setPadding(false);
-
-        Details details = new Details("Contact information", content);
-        details.setOpened(true);
-
-        leftSideContainerLayout.add(details);
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
 
 
+        CoreRealm targetCoreRealm = RealmTermFactory.getDefaultCoreRealm();
+        targetCoreRealm.openGlobalSession();
+        try {
+            SystemMaintenanceOperator systemMaintenanceOperator = targetCoreRealm.getSystemMaintenanceOperator();
+            ZonedDateTime currentDateTime = ZonedDateTime.now();
+            if(systemMaintenanceOperator != null){
+                List<EntityStatisticsInfo> realtimeConceptionList = targetCoreRealm.getConceptionEntitiesStatistics();
+                List<EntityStatisticsInfo> realtimeRelationList = targetCoreRealm.getRelationEntitiesStatistics();
 
+                Map<String, List<AttributeSystemInfo>> conceptionKindsAttributesSystemInfo = systemMaintenanceOperator.getPeriodicCollectedConceptionKindsAttributesSystemRuntimeInfo();
+                if(conceptionKindsAttributesSystemInfo == null || conceptionKindsAttributesSystemInfo.isEmpty()){
+                    conceptionKindsAttributesSystemInfo = systemMaintenanceOperator.getAllConceptionKindsAttributesSystemInfo();
+                }else{
+                    ZonedDateTime _latestRecordDatetime = conceptionKindsAttributesSystemInfo.values().iterator().next().get(0).getCreateDate();
+                    ZonedDateTime oneDayBeforeCurrent = currentDateTime.minusDays(1);
+                    if(_latestRecordDatetime.isBefore(oneDayBeforeCurrent)){
+                        conceptionKindsAttributesSystemInfo = systemMaintenanceOperator.getAllConceptionKindsAttributesSystemInfo();
+                    }
+                }
+
+
+                Map<String, List<AttributeSystemInfo>> relationKindsAttributesSystemInfo = systemMaintenanceOperator.getPeriodicCollectedRelationKindsAttributesSystemRuntimeInfo();
+                if(relationKindsAttributesSystemInfo == null || relationKindsAttributesSystemInfo.isEmpty()){
+                    relationKindsAttributesSystemInfo = systemMaintenanceOperator.getAllRelationKindsAttributesSystemInfo();
+                }else{
+                    ZonedDateTime _latestRecordDatetime = relationKindsAttributesSystemInfo.values().iterator().next().get(0).getCreateDate();
+                    ZonedDateTime oneDayBeforeCurrent = currentDateTime.minusDays(1);
+                    if(_latestRecordDatetime.isBefore(oneDayBeforeCurrent)){
+                        relationKindsAttributesSystemInfo = systemMaintenanceOperator.getAllRelationKindsAttributesSystemInfo();
+                    }
+                }
+
+                List<ConceptionKindCorrelationInfo> conceptionKindCorrelationInfoList = systemMaintenanceOperator.getPeriodicCollectedConceptionKindCorrelationRuntimeInfo(SystemMaintenanceOperator.PeriodicCollectedInfoRetrieveLogic.LATEST);
+                if(conceptionKindCorrelationInfoList == null || conceptionKindCorrelationInfoList.isEmpty()){
+                    conceptionKindCorrelationInfoList = systemMaintenanceOperator.getConceptionKindCorrelationRuntimeInfo(1);
+                }else{
+                    ZonedDateTime _latestRecordDatetime = conceptionKindCorrelationInfoList.get(0).getCreateDate();
+                    ZonedDateTime oneDayBeforeCurrent = currentDateTime.minusDays(1);
+                    if(_latestRecordDatetime.isBefore(oneDayBeforeCurrent)){
+                        conceptionKindCorrelationInfoList = systemMaintenanceOperator.getConceptionKindCorrelationRuntimeInfo(1);
+                    }
+                }
+            }
+        } catch (CoreRealmServiceEntityExploreException e) {
+            throw new RuntimeException(e);
+        } catch (CoreRealmServiceRuntimeException e) {
+            throw new RuntimeException(e);
+        } finally {
+            targetCoreRealm.closeGlobalSession();
+        }
     }
 }
